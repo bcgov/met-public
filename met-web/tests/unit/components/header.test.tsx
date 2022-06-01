365d5644cebc31e7f921219a9f864e6534f39de0
Tiago Graf <tiagograf@gmail.com>
2022-05-31 20:54:38 -0700

    Adds unit tests to web CI

---
+++ met-web/tests/unit/components/header.test.tsx
@@ -0,0 +1,12 @@
+import { screen } from "@testing-library/dom";
+import { render } from "@testing-library/react";
+
+describe("The Header", () => {
+  beforeEach(() => {
+    // initialize before each test
+  });
+
+  it("Basic Test", () => {
+    expect(0).toBe(0)
+  });
+});
