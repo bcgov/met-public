18765e1b4a39c2ad8555cafee8224af50f92fa69
Jad <jad.saad@aot-technologies.com>
2022-06-06 16:27:41 -0700

    Load env variables from public config file

--- met-web/src/config.ts
+++ met-web/src/config.ts
@@ -1,8 +1,14 @@
-let PROJECT_URL = process.env.REACT_APP_API_PROJECT_URL || 'https://formio-e903c2-dev.apps.gold.devops.gov.bc.ca';
-let API_URL = process.env.REACT_APP_API_SERVER_URL || 'https://formio-e903c2-dev.apps.gold.devops.gov.bc.ca';
+declare global {
+    interface Window {
+        _env_: {
+            REACT_APP_API_PROJECT_URL: string;
+        };
+    }
+}
 
-PROJECT_URL = PROJECT_URL;
-API_URL = API_URL;
+const PROJECT_URL = window._env_.REACT_APP_API_PROJECT_URL || process.env.REACT_APP_API_PROJECT_URL;
+
+const API_URL = window._env_.REACT_APP_API_PROJECT_URL || process.env.REACT_APP_API_SERVER_URL;
 
 export const AppConfig = {
     projectUrl: PROJECT_URL,
