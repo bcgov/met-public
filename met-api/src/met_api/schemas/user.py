78f33a3e4490ad4d4473094cfb799db31b423443
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2022-06-21 13:07:08 -0400

    cleanup

--- met-api/src/met_api/schemas/user.py
+++ met-api/src/met_api/schemas/user.py
@@ -17,7 +17,6 @@ class UserSchema(Schema):
     id = fields.Int(data_key='id')
     first_name = fields.Str(data_key='first_name')
     middle_name = fields.Str(data_key='description')
-    status_name = fields.Str(data_key="status_name")
     last_name = fields.Str(data_key='last_name')
     email_id = fields.Int(data_key='email_id')
     contact_number = fields.Str(data_key='contact_number')
