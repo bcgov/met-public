21b06873b65ae9b8c70addb7560f562e8ac6777b
Jad <jad.saad@aot-technologies.com>
2022-06-21 09:23:50 -0700

    Improve banner with image and include error handling

--- met-web/src/components/engagement/view/BannerWithImage.tsx
+++ met-web/src/components/engagement/view/BannerWithImage.tsx
@@ -1,33 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Grid, Box, Typography } from '@mui/material';
 import { ActionContext } from './ActionContext';
 import { formatDate } from '../../common/dateHelper';
+import BannerWithoutImage from './BannerWithoutImage';
 
 const BannerWithImage = () => {
-    //Just a place holder image
-    const imageLink =
-        'https://images.unsplash.com/photo-1532601224476-15c79f2f7a51?ixlib=rb-1.2.1&w=1200&fit=crop&q=60&fm=jpg&crop=faces%2Cedges&cs=tinysrgb&auto=format&h=630&mark-w=64&mark-align=top%2Cleft&mark-pad=50&blend-mode=normal&blend-alpha=10&blend-w=1&mark=https%3A%2F%2Fimages.unsplash.com%2Fopengraph%2Flogo.png&blend=000000';
-    const imageHeight = '20em';
-
     const { savedEngagement } = useContext(ActionContext);
 
     const { description, name, start_date, end_date, banner_image_link } = savedEngagement;
+    const [imageError, setImageError] = useState(false);
+
+    if (imageError) {
+        return <BannerWithoutImage />;
+    }
+
     return (
-        <Box
-            sx={{
-                height: imageHeight,
-                width: '100%',
-                background: ` url(${banner_image_link}) no-repeat`,
-                backgroundSize: `cover`,
-            }}
-        >
+        <>
             <Box
                 sx={{
-                    backgroundColor: 'rgba(96, 96, 96, 0.7)',
-                    height: '100%',
+                    height: '20em',
                     width: '100%',
+                    position: 'relative',
                 }}
             >
+                <img
+                    src={banner_image_link}
+                    style={{
+                        objectFit: 'cover',
+                        height: '20em',
+                        width: '100%',
+                    }}
+                    onError={() => setImageError(true)}
+                />
+
                 <Grid
                     container
                     direction="column"
@@ -35,12 +40,18 @@ const BannerWithImage = () => {
                     alignItems="flex-start"
                     height="100%"
                     padding="5em 5em 0 3em"
+                    sx={{
+                        backgroundColor: 'rgba(96, 96, 96, 0.7)',
+                        position: 'absolute',
+                        top: '0px',
+                        left: '0px',
+                    }}
                 >
                     <Grid item xs={10}>
                         <Typography color="white" variant="h3">
                             {name}
                         </Typography>
-                        <Typography color="white" variant="subtitle2">
+                        <Typography color="white" variant="subtitle2" style={{ fontWeight: 600 }}>
                             {description}
                         </Typography>
                     </Grid>
@@ -54,7 +65,7 @@ const BannerWithImage = () => {
                     </Grid>
                 </Grid>
             </Box>
-        </Box>
+        </>
     );
 };
 
