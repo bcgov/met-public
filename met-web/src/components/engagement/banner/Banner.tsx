51203ba3e119036a8767faef704e695d145c8d6e
Jad <jad.saad@aot-technologies.com>
2022-07-12 13:33:21 -0700

    revert debug

--- met-web/src/components/engagement/banner/Banner.tsx
+++ met-web/src/components/engagement/banner/Banner.tsx
@@ -4,7 +4,7 @@ import BannerWithoutImage from './BannerWithoutImage';
 import { EngagementBannerProps } from '../view/types';
 
 export const Banner = ({ savedEngagement, children }: EngagementBannerProps) => {
-    const imageExists = false;
+    const imageExists = !!savedEngagement.banner_url;
 
     if (imageExists) {
         return <BannerWithImage savedEngagement={savedEngagement}>{children}</BannerWithImage>;
