356b202277140a24ca9dcc740ae869e4912170ff
djnunez-aot <david.nunez@aot-technologies.com>
2022-07-18 13:53:23 -0400

    Fix Conflicts

--- met-web/src/components/engagement/view/EngagementBanner.tsx
+++ met-web/src/components/engagement/view/EngagementBanner.tsx
@@ -4,6 +4,7 @@ import { Button, Grid, Skeleton } from '@mui/material';
 import { Banner } from '../banner/Banner';
 import { useNavigate } from 'react-router-dom';
 import EmailModal from 'components/survey/submit/EmailModal';
+import { ConditionalComponent } from 'components/common';
 
 export const EngagementBanner = () => {
     const navigate = useNavigate();
@@ -11,18 +12,21 @@ export const EngagementBanner = () => {
     const surveyId = savedEngagement.surveys[0]?.id || '';
     const [open, setOpen] = useState(true);
 
+    const publishedStatus = savedEngagement.engagement_status.id === 2;
     if (engagementLoading) {
         return <Skeleton variant="rectangular" width="100%" height="35em" />;
     }
 
     return (
         <Banner savedEngagement={savedEngagement}>
-            <Grid item xs={12} container direction="row" justifyContent="flex-end">
-                <Button variant="contained" onClick={() => setOpen(true)}>
-                    Share your thoughts
-                </Button>
-                <EmailModal open={open} handleClose={() => setOpen(false)} />
-            </Grid>
+            <ConditionalComponent condition={!!surveyId && publishedStatus}>
+                <Grid item xs={12} container direction="row" justifyContent="flex-end">
+                    <Button variant="contained" onClick={() => setOpen(true)}>
+                        Share your thoughts
+                    </Button>
+                    <EmailModal open={open} handleClose={() => setOpen(false)} />
+                </Grid>
+            </ConditionalComponent>
         </Banner>
     );
 };
