3a34f5fcec438a419f7fedf81e193f1c282f5f31
djnunez-aot <david.nunez@aot-technologies.com>
2022-07-19 12:41:16 -0400

    refactor modal

---
+++ met-web/src/components/engagement/view/EmailModal.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Button, Modal, Box, Typography, TextField, Checkbox } from '@mui/material';
+import SuccessPanel from './SuccessPanel';
+import FailurePanel from './FailurePanel';
+import EmailPanel from './EmailPanel';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { EmailModalProps } from './types';
+
+function EmailModal(props: EmailModalProps) {
+    const [formIndex, setFormIndex] = useState('email');
+    const [email, setEmail] = useState('');
+
+    function handleClose() {
+        props.handleClose();
+        setFormIndex('email');
+    }
+
+    useEffect(() => {
+        console.log('EMAIL UPDATED::::' + email);
+    }, [email]);
+
+    function checkEmail() {
+        const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        console.log('EMAIL STRING::::::::::::::::' + JSON.stringify(email));
+        if (!filter.test(email)) {
+            setFormIndex('error');
+        } else {
+            setFormIndex('success');
+        }
+    }
+
+    return (
+        <>
+            <Modal
+                open={props.open}
+                onClose={props.handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <TabContext value={formIndex}>
+                    <TabPanel value="email">
+                        <EmailPanel
+                            email={email}
+                            checkEmail={checkEmail}
+                            handleClose={() => handleClose()}
+                            updateEmail={setEmail}
+                        />
+                    </TabPanel>
+                    <TabPanel value="success">
+                        <SuccessPanel open={props.open} handleClose={() => handleClose()} email={email} />
+                    </TabPanel>
+                    <TabPanel value="error">
+                        <FailurePanel
+                            open={props.open}
+                            tryAgain={() => setFormIndex('email')}
+                            handleClose={() => handleClose()}
+                            email={email}
+                        />
+                    </TabPanel>
+                </TabContext>
+            </Modal>
+        </>
+    );
+}
+
+export default EmailModal;
