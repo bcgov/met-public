b3caf0cc0ff76a7348e5162ba291ee408b96857a
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2022-07-20 11:27:11 -0700

    Analytics DB - multiple sql alchemy binds (#324)

--- met-etl/src/met_etl/models/survey.py
+++ met-etl/src/met_etl/models/survey.py
@@ -9,7 +9,9 @@ from .db import db
 class Survey(db.Model):  # pylint: disable=too-few-public-methods
     """Definition of the Survey entity."""
 
+    __bind_key__ = 'met_db_analytics'
     __tablename__ = 'survey'
+    
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), index=True)
@@ -18,6 +20,7 @@ class Survey(db.Model):  # pylint: disable=too-few-public-methods
     updated_date = db.Column(db.DateTime, onupdate=datetime.utcnow)
     created_by = db.Column(db.String(50))
     updated_by = db.Column(db.String(50))
+    user_id = db.Column(db.String(50))
 
     @classmethod
     def get_all(cls):
