0226cd9b88f1e24122b5268cac4a3d6b99a3d717
djnunez-aot <david.nunez@aot-technologies.com>
2022-07-27 14:56:03 -0400

    comment removal

--- met-web/tests/unit/components/header.test.tsx
+++ met-web/tests/unit/components/header.test.tsx
@@ -6,30 +6,23 @@ import ProviderShell from './ProviderShell';
 import { setupEnv } from './setEnvVars';
 
 test('Load Header', async () => {
-    // Arrange
-    // Act
-    // Assert
+
     setupEnv();
     render(
         <ProviderShell>
             <LoggedInHeader />
         </ProviderShell>,
     );
-    fireEvent.click(screen.getByTestId('button-header'));
 
-    // wait until the `get` request promise resolves and
-    // the component calls setState and re-renders.
-    // `waitFor` waits until the callback doesn't throw an error
+
 
     await waitFor(() =>
-        // getByRole throws an error if it cannot find an element
+
         screen.getByTestId('button-header'),
     );
-    // assert that the alert message is correct using
-    // toHaveTextContent, a custom matcher from jest-dom.
+
     expect(screen.getByTestId('button-header')).toHaveTextContent('Logout');
 
-    // assert that the button is not disabled using
-    // toBeDisabled, a custom matcher from jest-dom.
+
     expect(screen.getByTestId('button-header')).not.toBeDisabled();
 });
