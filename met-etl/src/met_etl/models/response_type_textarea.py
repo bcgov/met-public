18a632318fdde048726640f5096edeb4b1c0dea1
VineetBala-AOT <90332175+VineetBala-AOT@users.noreply.github.com>
2022-08-03 15:42:14 -0600

    Update response_type_textarea.py

--- met-etl/src/met_etl/models/response_type_textarea.py
+++ met-etl/src/met_etl/models/response_type_textarea.py
@@ -5,7 +5,7 @@ Manages the responses for a textarea type questions on a survey
 from sqlalchemy import ForeignKeyConstraint
 from .db import db
 from .base_model import BaseModel
-from .request_type_textarea import request_type_textarea
+from .request_type_textarea import RequestTypeTextarea
 
 
 class ResponseTypeTextarea(BaseModel):  # pylint: disable=too-few-public-methods
@@ -22,5 +22,5 @@ class ResponseTypeTextarea(BaseModel):  # pylint: disable=too-few-public-methods
     user_id = db.Column(db.Integer)
     value = db.Column(db.String(5000)) 
     __table_args__ = (ForeignKeyConstraint([request_id, request_key, survey_id, engagement_id],
-                                           [request_type_textarea.id, request_type_textarea.key, request_type_textarea.survey_id, request_type_textarea.engagement_id]),
+                                           [RequestTypeTextarea.id, RequestTypeTextarea.key, RequestTypeTextarea.survey_id, RequestTypeTextarea.engagement_id]),
                       {}) 
