29d824142267fe933813a44ce31691317f509489
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2022-08-09 17:06:35 -0700

    etl service split into layers

--- met-etl/src/met_etl/services/engagement_etl_service.py
+++ met-etl/src/met_etl/services/engagement_etl_service.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Service to do ETL on e."""
+from datetime import datetime, timedelta
 from typing import List
 
 from flask import current_app
 from met_api.models.engagement import Engagement as MetEngagementModel
-
+from met_api.constants.engagement_status import Status as EngagementStatus
 from met_etl.models.db import db
 from met_etl.models.engagement import Engagement as EtlEngagementModel
 
@@ -25,7 +26,7 @@ class EngagementEtlService:  # pylint: disable=too-few-public-methods
     """ETL Service on Engagement."""
 
     @staticmethod
-    def do_etl_engagement(updated_engagements: List[MetEngagementModel]):
+    def do_etl_engagement():
         """Perform the ETL for Engagement.
 
         1). Find engagements
@@ -35,18 +36,30 @@ class EngagementEtlService:  # pylint: disable=too-few-public-methods
         3). If Yes, Inactivate the existing one , Create new  one
         4). If No, create it
         """
+        time_delta_in_minutes: int = int(current_app.config.get('TIME_DELTA_IN_MINUTES'))
+        time_delta = datetime.utcnow() - timedelta(minutes=time_delta_in_minutes)
+
+        updated_engagements = db.session.query(MetEngagementModel).filter(
+            MetEngagementModel.updated_date > time_delta,
+            MetEngagementModel.status_id != EngagementStatus.Draft.value).all()
+        if not updated_engagements:
+            current_app.logger.info('No updated Engagement Found')
+            return
+
+        current_app.logger.info('Total updated Engagement Found : %s.', len(updated_engagements))
         engagement: MetEngagementModel
         for engagement in updated_engagements:
-            existing_eng: EtlEngagementModel
             current_app.logger.info('Processing updated Engagement: %s.', engagement.id)
+            existing_eng: EtlEngagementModel
             if existing_eng := EtlEngagementModel.find_by_id(engagement.id):
                 # deactivate the existing one
                 current_app.logger.info('Found existing engagement Engagement in Analytics DB: %s.', engagement.id)
-                existing_eng.active_flag = 'M'
+                existing_eng.active_flag = 'N'
                 db.session.add(existing_eng)
-            eng = EngagementEtlService._build_engagement(engagement)
-            db.session.add(eng)
-            db.session.commit()
+            new_eng = EngagementEtlService._build_engagement(engagement)
+            db.session.add(new_eng)
+            current_app.logger.info('Created new engagement Engagement in Analytics DB: %s.', new_eng.id)
+        db.session.commit()
 
     @staticmethod
     def _build_engagement(engagement):
