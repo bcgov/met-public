054f3b03165900034b92974e65fa5f60f607e3a0
jadmsaadaot <91914654+jadmsaadaot@users.noreply.github.com>
2022-08-10 09:55:16 -0700

    Revert body1 changes (#420)

--- met-web/src/styles/Theme.ts
+++ met-web/src/styles/Theme.ts
@@ -84,10 +84,6 @@ export const BaseTheme = createTheme({
             fontWeight: 500,
             fontSize: '16px',
         },
-        body2: {
-            fontWeight: 500,
-            fontSize: '0.8rem',
-        },
         button: {
             fontWeight: 700,
             fontSize: '1.2rem',
