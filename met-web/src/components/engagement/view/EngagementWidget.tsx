f6b1bb1a322992515bc73212f21a229c7ee5f4ea
djnunez-aot <david.nunez@aot-technologies.com>
2022-08-12 15:12:47 -0400

    cleanup

--- met-web/src/components/engagement/view/EngagementWidget.tsx
+++ met-web/src/components/engagement/view/EngagementWidget.tsx
@@ -13,8 +13,8 @@ export default function EngagementWidget() {
     return (
         <Grid container alignItems="center" justifyContent="center" spacing={2} xs={12}>
             <Grid item container xs={11}>
-                <MetPaper elevation={1} sx={{ padding: '1em', minHeight: '10em' }}>
-                    <Grid item justifyContent="flex-start" alignItems="center" xs={12}>
+                <MetPaper elevation={1} sx={{ padding: '1em', minHeight: '12em' }}>
+                    <Grid item sx={{ pb: 2 }} justifyContent="flex-start" alignItems="center" xs={12}>
                         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
                             Who is Listening
                         </Typography>
@@ -24,12 +24,22 @@ export default function EngagementWidget() {
                             <Avatar
                                 alt={testData.name}
                                 src={testData.image}
-                                sx={{ borderRadius: '0px', height: '80%', width: '100%' }}
+                                sx={{ borderRadius: '0px', height: '100%', width: '100%' }}
                             />
                         </Grid>
-                        <Grid container item alignItems="flex-start" justifyContent="center" direction="column" xs={8}>
+                        <Grid
+                            container
+                            item
+                            alignItems="flex-start"
+                            justifyContent="center"
+                            direction="column"
+                            spacing={1}
+                            xs={8}
+                        >
                             <Grid item xs={3}>
-                                {testData.name}
+                                <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                    {testData.name}
+                                </Typography>
                             </Grid>
                             <Grid item xs={3}>
                                 <Link href={`mailto:${testData.email}`}>{testData.email}</Link>
