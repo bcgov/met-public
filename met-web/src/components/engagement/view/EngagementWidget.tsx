0702ea17360782b82ff310fdd14d27cbf1055141
djnunez-aot <david.nunez@aot-technologies.com>
2022-08-12 13:45:07 -0400

    update format

--- met-web/src/components/engagement/view/EngagementWidget.tsx
+++ met-web/src/components/engagement/view/EngagementWidget.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { MetPaper } from 'components/common';
 import { Grid, Typography, Avatar, Link } from '@mui/material';
-import TheJad from 'assets/images/The_Jad.png';
+
+const testData = {
+    image: 'https://cdn.pixabay.com/photo/2017/09/25/13/12/cocker-spaniel-2785074__340.jpg',
+    name: 'Remy Sharp',
+    email: 'TheJad@bc.gov.ca',
+    phone: '647-543-1332',
+};
+
 export default function EngagementWidget() {
     return (
         <Grid container alignItems="center" justifyContent="center" spacing={2} xs={12}>
@@ -15,20 +22,20 @@ export default function EngagementWidget() {
                     <Grid container spacing={1} item xs={12}>
                         <Grid item container alignItems="center" justifyContent="center" xs={4}>
                             <Avatar
-                                alt="Remy Sharp"
-                                src={TheJad}
+                                alt={testData.name}
+                                src={testData.image}
                                 sx={{ borderRadius: '0px', height: '80%', width: '100%' }}
                             />
                         </Grid>
                         <Grid container item alignItems="flex-start" justifyContent="center" direction="column" xs={8}>
                             <Grid item xs={3}>
-                                <strong>Remy Sharp</strong>
+                                <strong>{testData.name}</strong>
                             </Grid>
                             <Grid item xs={3}>
-                                <Link>TheJad@hotmail.com</Link>
+                                <Link>{testData.email}</Link>
                             </Grid>
                             <Grid item xs={2}>
-                                Phone: <Link>647-543-1332</Link>
+                                Phone: <Link>{testData.phone}</Link>
                             </Grid>
                         </Grid>
                     </Grid>
