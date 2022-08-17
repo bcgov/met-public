d97ff06c23bbf523c441a3c0025992a964b358bb
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2022-08-16 17:06:03 -0700

    fixed the comments

--- met-cron/tasks/met_extractor.py
+++ met-cron/tasks/met_extractor.py
@@ -14,7 +14,9 @@
 """MET Extractions."""
 from datetime import datetime
 
+from met_cron.services.engagement_etl_service import EngagementEtlService
 from met_cron.services.submission_etl_service import SubmissionEtlService
+from met_cron.services.survey_etl_service import SurveyEtlService
 
 
 class MetExtractor:  # pylint:disable=too-few-public-methods
