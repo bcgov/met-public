0df33efc1e09d96091beb4a32a285966572a8e27
Jad <jad.saad@aot-technologies.com>
2022-08-17 15:47:18 -0700

    fix lint issues

--- met-api/src/met_api/services/comment_service.py
+++ met-api/src/met_api/services/comment_service.py
@@ -80,11 +80,11 @@ class CommentService:
     def review_comment(cls, comment_id, status_id, external_user_id):
         """Review comment."""
         user = UserService.get_user_by_external_id(external_user_id)
-        
+
         valid_statuses = [status.id for status in CommentStatus.get_comment_statuses()]
-        
+
         if not status_id or status_id == 1 or status_id not in valid_statuses or not user:
-            raise ValueError('Invalid review')        
+            raise ValueError('Invalid review')
 
         comment = cls.get_comment(comment_id)
         if not comment:
