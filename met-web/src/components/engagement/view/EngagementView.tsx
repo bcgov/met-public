1bd284f8020e34986cd232f5f78a4a72b257a39e
djnunez-aot <david.nunez@aot-technologies.com>
2022-08-18 17:04:03 -0400

    code smell cleanup

--- met-web/src/components/engagement/view/EngagementView.tsx
+++ met-web/src/components/engagement/view/EngagementView.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import { Grid } from '@mui/material';
 import { EngagementBanner } from './EngagementBanner';
 import { ActionContext } from './ActionContext';
