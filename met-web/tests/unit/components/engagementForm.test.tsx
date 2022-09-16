8294119a2cd8d04b4fdf86effaf649e81fa45b79
Jad <jad.saad@aot-technologies.com>
2022-09-16 15:45:23 -0700

    reArrange fields in mock data

--- met-web/tests/unit/components/engagementForm.test.tsx
+++ met-web/tests/unit/components/engagementForm.test.tsx
@@ -14,26 +14,26 @@ import { EngagementStatus } from 'constants/engagementStatus';
 
 const mockSurvey = {
     ...createDefaultSurvey(),
-    engagement_id: '1',
     id: 1,
     name: 'Survey 1',
+    engagement_id: '1',
 };
 
 const mockSurveys = [mockSurvey];
 
 const mockEngagement = {
     ...createDefaultEngagement(),
-    content: 'Test content',
-    created_date: '2022-09-14 20:16:29.846877',
-    description: 'Test description',
-    end_date: '2022-09-30',
     id: 1,
     name: 'Test Engagement',
+    created_date: '2022-09-14 20:16:29.846877',
     rich_content:
         '{"blocks":[{"key":"29p4m","text":"Test content","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+    content: 'Test content',
     rich_description:
         '{"blocks":[{"key":"bqupg","text":"Test description","type":"unstyled","depth":0,"inlineStyleRanges":[],"entityRanges":[],"data":{}}],"entityMap":{}}',
+    description: 'Test description',
     start_date: '2022-09-01',
+    end_date: '2022-09-30',
     surveys: mockSurveys,
     engagement_status: {
         id: EngagementStatus.Draft,
