ab9cd48380f68dd6b4e541e9df3149cf81dedf4a
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2022-10-04 12:56:58 -0400

    Update BannerWithoutImage.tsx

--- met-web/src/components/engagement/banner/BannerWithoutImage.tsx
+++ met-web/src/components/engagement/banner/BannerWithoutImage.tsx
@@ -62,7 +62,7 @@ const BannerWithoutImage = ({ savedEngagement }: BannerProps) => {
                         rowSpacing={2}
                     >
                         <Grid item xs={12} sx={{ maxHeight: '20em', overflowY: 'auto', overflowX: 'auto' }}>
-                            <MetHeader1 color="black">{name}</MetHeader1>
+                            <MetHeader1>{name}</MetHeader1>
                             <Editor editorState={getEditorState(rich_description)} readOnly={true} toolbarHidden />
                         </Grid>
                         <Grid item xs={12}>
