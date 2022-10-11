57753939e94b4e761fe267aa5ac20c70ffefb8dd
Jad <jad.saad@aot-technologies.com>
2022-10-11 13:44:47 -0700

    Change who is listening grid

--- met-web/src/components/engagement/form/EngagementWidgets/WhoIsListeningForm.tsx
+++ met-web/src/components/engagement/form/EngagementWidgets/WhoIsListeningForm.tsx
@@ -1,11 +1,11 @@
 import React, { useContext, useState } from 'react';
-import { Autocomplete, Grid, Stack, TextField } from '@mui/material';
+import { Autocomplete, Grid, TextField } from '@mui/material';
 import { ActionContext } from '../ActionContext';
-import { MetLabel, SecondaryButton } from 'components/common';
+import { MetLabel, PrimaryButton, SecondaryButton } from 'components/common';
 import { WidgetContact } from '../types';
 
 const WhoIsListeningForm = () => {
-    const { handleWidgetDrawerTabValueChange } = useContext(ActionContext);
+    const { handleWidgetDrawerOpen } = useContext(ActionContext);
     const [selectedContact, setSelectedContact] = useState<WidgetContact | null>(null);
 
     const testContact = {
@@ -18,35 +18,49 @@ const WhoIsListeningForm = () => {
         bio: 'Mandy is always happy to answer your questions and...',
     };
     return (
-        <Grid container item xs={12} alignItems="stretch" justifyContent={'flex-start'} spacing={1}>
+        <Grid item xs={12} container alignItems="flex-start" justifyContent={'flex-start'} spacing={1}>
             <Grid item xs={12}>
                 <MetLabel>Select Existing Contact</MetLabel>
             </Grid>
-            <Grid item xs={8}>
-                <Autocomplete
-                    id="contact-selector"
-                    options={[testContact]}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            label=" "
-                            InputLabelProps={{
-                                shrink: false,
-                            }}
-                            inputProps={{ sx: { height: '1em' } }}
-                        />
-                    )}
-                    getOptionLabel={(contact: WidgetContact) => contact.name}
-                    onChange={(_e: React.SyntheticEvent<Element, Event>, contact: WidgetContact | null) =>
-                        setSelectedContact(contact)
-                    }
-                    // disabled={loadingContacts}
-                />
+            <Grid item xs={12} container direction="row" justifyContent={'flex-start'} spacing={1}>
+                <Grid item xs={12}>
+                    <Autocomplete
+                        id="contact-selector"
+                        options={[testContact]}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label=" "
+                                InputLabelProps={{
+                                    shrink: false,
+                                }}
+                            />
+                        )}
+                        getOptionLabel={(contact: WidgetContact) => contact.name}
+                        onChange={(_e: React.SyntheticEvent<Element, Event>, contact: WidgetContact | null) =>
+                            setSelectedContact(contact)
+                        }
+                        // disabled={loadingContacts}
+                    />
+                </Grid>
+                <Grid item>
+                    <PrimaryButton sx={{ height: '100%' }} fullWidth>
+                        Add existing contact
+                    </PrimaryButton>
+                </Grid>
+                <Grid item>
+                    <SecondaryButton sx={{ height: '100%' }} fullWidth>
+                        Create New Contact
+                    </SecondaryButton>
+                </Grid>
             </Grid>
-            <Grid item xs={4}>
-                <SecondaryButton sx={{ height: '2em' }} fullWidth>
-                    Add New Contact
-                </SecondaryButton>
+            <Grid item xs={12} container direction="row" spacing={1} justifyContent={'flex-start'} marginTop="8em">
+                <Grid item>
+                    <PrimaryButton>{`Save & Close`}</PrimaryButton>
+                </Grid>
+                <Grid item>
+                    <SecondaryButton onClick={() => handleWidgetDrawerOpen(false)}>{`Cancel`}</SecondaryButton>
+                </Grid>
             </Grid>
         </Grid>
     );
