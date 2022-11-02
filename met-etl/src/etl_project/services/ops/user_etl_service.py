365d35bd375698030666c595d3b3e63dc7358105
VineetBala-AOT <90332175+VineetBala-AOT@users.noreply.github.com>
2022-11-02 13:03:01 -0700

    etl re-write (#810)
    * updating etlruncycle model
    
    * user detail etl

--- met-etl/src/etl_project/services/ops/user_etl_service.py
+++ met-etl/src/etl_project/services/ops/user_etl_service.py
@@ -1,49 +1,52 @@
-from typing import Dict, List
+from dagster import Out, Output, op
+from met_api.models.user import User as MetUserModel
+from met_cron.models.user_details import UserDetails as UserDetailsModel
+from met_cron.models.etlruncycle import EtlRunCycle as EtlRunCycleModel
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+from utils.config import get_met_db_creds, get_met_analytics_db_creds
+from datetime import datetime, timedelta
 
-import psycopg2.extras as p
-from dagster import op
-from utils.config import (
-    get_met_db_creds,
-    get_met_analytics_db_creds,
-)
-from utils.db import WarehouseConnection
+@op(out={"userdetailslastruncycledatetime": Out(is_required=True)})
+def get_user_details_last_run_cycle_datetime(context):
+    metanalyticsdbengine = get_met_analytics_db_creds()
+    Session = sessionmaker(bind=metanalyticsdbengine)
+    metanalyticsdbsession = Session()
+    defaultdatetime = datetime(1900, 1, 1, 15, 59, 56, 721228)
+    user_details_last_run_cycle_datetime = metanalyticsdbsession.query(func.coalesce(func.max(EtlRunCycleModel.enddatetime), defaultdatetime)).filter(EtlRunCycleModel.packagename == 'userdetails', EtlRunCycleModel.success == True).first()
+    yield Output(user_details_last_run_cycle_datetime, "userdetailslastruncycledatetime")
+    for lastruncycledatetime in user_details_last_run_cycle_datetime:
+        if lastruncycledatetime == defaultdatetime:
+            context.log.info("No record found in run cycle table")
+            metanalyticsdbsession.add(EtlRunCycleModel(id = 1, packagename = 'userdetails', startdatetime = datetime.utcnow(), enddatetime = None, description='started the load for user_details table', success = False))
+        if lastruncycledatetime != defaultdatetime:
+            context.log.info("Latest record found in run cycle table")
+            for lastruncycledatetime in user_details_last_run_cycle_datetime:
+                user_details_last_run_cycle_id = metanalyticsdbsession.query(EtlRunCycleModel.id).filter(EtlRunCycleModel.enddatetime == lastruncycledatetime).first()
+                for lastruncycleid in user_details_last_run_cycle_id:
+                    new_run_cycle_id = lastruncycleid + 1
+                    metanalyticsdbsession.add(EtlRunCycleModel(id = new_run_cycle_id, packagename = 'userdetails', startdatetime = datetime.utcnow(), enddatetime = None, description='started the load for user_details table', success = False))
+    metanalyticsdbsession.commit()
+    metanalyticsdbsession.close()
+
+
+@op(out={"newusers": Out(is_required=True)})
+def extract_user(context, userdetailslastruncycledatetime):
+    engine = get_met_db_creds()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_users = session.query(MetUserModel).filter(MetUserModel.created_date > userdetailslastruncycledatetime).all()
+    context.log.info(new_users)
+    yield Output(new_users, "newusers")
+    session.commit()
+    session.close()
 
 @op
-def extract_user_data() -> List[Dict[str, str]]:
-    with WarehouseConnection(get_met_db_creds()).managed_cursor() as curr:
-        curr.execute(
-            '''
-            select 
-                'Y' as is_active,
-                email_id as name,
-                1 as runcycle_id
-            from public.user
-            '''
-        )
-        user_data = curr.fetchall()
-    return [
-        {
-            "is_active": str(d[0]),
-            "name": str(d[1]),
-            "runcycle_id": str(d[2]),
-        }
-        for d in user_data
-    ]
-
-@op
-def load_user_data(user_data_to_load: List[Dict[str, str]]):
-    ins_qry = """
-    INSERT INTO public.user_details(
-        is_active,
-        name,
-        runcycle_id
-    )
-    VALUES (
-        %(is_active)s,
-        %(name)s,
-        %(runcycle_id)s
-    )
-    """
-    with WarehouseConnection(get_met_analytics_db_creds()).managed_cursor() as curr:
-        p.execute_batch(curr, ins_qry, user_data_to_load)
-
+def load_new_user(context, newusers):
+    engine = get_met_analytics_db_creds()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for user in newusers:
+        session.add(UserDetailsModel(name = user.external_id, is_active = True, created_date = user.created_date, updated_date = user.updated_date, runcycle_id = 1))
+        session.commit()
+    session.close()
--- met-etl/src/etl_project/services/utils/config.py
+++ met-etl/src/etl_project/services/utils/config.py
@@ -1,23 +1,26 @@
 import os
+from sqlalchemy import create_engine
 
-from utils.db import DBConnection
+def get_met_db_creds():
+    user=os.getenv("MET_DB_USER", "")
+    password=os.getenv("MET_DB_PASSWORD", "")
+    db=os.getenv("MET_DB_DB", "")
+    host=os.getenv("MET_DB_HOST", "")
+    port=int(os.getenv("MET_DB_PORT", 54332))
+    DBConnection = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    try:
+        return DBConnection
+    except:
+        print("Error loading the config file.")
 
-
-def get_met_analytics_db_creds() -> DBConnection:
-    return DBConnection(
-        user=os.getenv("MET_ANALYTICS_DB_USER", ""),
-        password=os.getenv("MET_ANALYTICS_DB_PASSWORD", ""),
-        db=os.getenv("MET_ANALYTICS_DB_DB", ""),
-        host=os.getenv("MET_ANALYTICS_DB_HOST", ""),
-        port=int(os.getenv("MET_ANALYTICS_DB_PORT", 54334)),
-    )
-
-
-def get_met_db_creds() -> DBConnection:
-    return DBConnection(
-        user=os.getenv("MET_DB_USER", ""),
-        password=os.getenv("MET_DB_PASSWORD", ""),
-        db=os.getenv("MET_DB_DB", ""),
-        host=os.getenv("MET_DB_HOST", ""),
-        port=int(os.getenv("MET_DB_PORT", 5432)),
-    )
+def get_met_analytics_db_creds():
+    user=os.getenv("MET_ANALYTICS_DB_USER", "")
+    password=os.getenv("MET_ANALYTICS_DB_PASSWORD", "")
+    db=os.getenv("MET_ANALYTICS_DB_DB", "")
+    host=os.getenv("MET_ANALYTICS_DB_HOST", "")
+    port=os.getenv("MET_ANALYTICS_DB_PORT", 54334)
+    DBConnection = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    try:
+        return DBConnection
+    except:
+        print("Error loading the config file.")
