d57a739a2c042ff2ad0c55630d21ff23e0a50eb8
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2022-11-03 16:24:43 -0700

    updated etl job

--- met-etl/src/etl_project/services/jobs/met_data_ingestion.py
+++ met-etl/src/etl_project/services/jobs/met_data_ingestion.py
@@ -6,12 +6,12 @@ from ops.engagement_etl_service import extract_engagement, load_engagement
 
 @job
 def met_data_ingestion():
-    survey_last_run_cycle_time, survey_new_runcycleid_step1 = get_survey_last_run_cycle_time()
-    new_survey, updated_survey, survey_new_runcycleid_step2 = extract_survey(survey_last_run_cycle_time, survey_new_runcycleid_step1)
-    survey_new_runcycleid_step3 = load_survey(new_survey, updated_survey, survey_new_runcycleid_step2)
-    flag_to_run_step_after_survey = survey_end_run_cycle(survey_new_runcycleid_step3)
-    submission_last_run_cycle_time, submission_new_runcycleid_step1 = get_submission_last_run_cycle_time(flag_to_run_step_after_survey)
-    new_submission, updated_submission, submission_new_runcycleid_step2 = extract_submission(submission_last_run_cycle_time, submission_new_runcycleid_step1)
-    submission_new_runcycleid_step3 = load_submission(new_submission, updated_submission, submission_new_runcycleid_step2)
-    submission_new_runcycleid_step4 = load_user_response_details(new_submission, updated_submission, submission_new_runcycleid_step3)
-    flag_to_run_step_after_submission = submission_end_run_cycle(submission_new_runcycleid_step4)
+    survey_last_run_cycle_time, survey_new_runcycleid_created = get_survey_last_run_cycle_time()
+    new_survey, updated_survey, survey_new_runcycleid_passed_to_load = extract_survey(survey_last_run_cycle_time, survey_new_runcycleid_created)
+    survey_new_runcycleid_passed_to_end = load_survey(new_survey, updated_survey, survey_new_runcycleid_passed_to_load)
+    flag_to_run_step_after_survey = survey_end_run_cycle(survey_new_runcycleid_passed_to_end)
+    submission_last_run_cycle_time, submission_new_runcycleid_created = get_submission_last_run_cycle_time(flag_to_run_step_after_survey)
+    new_submission, updated_submission, submission_new_runcycleid_passed_to_load = extract_submission(submission_last_run_cycle_time, submission_new_runcycleid_created)
+    submission_new_runcycleid_passed_to_load_reponse = load_submission(new_submission, updated_submission, submission_new_runcycleid_passed_to_load)
+    submission_new_runcycleid_passed_to_end = load_user_response_details(new_submission, updated_submission, submission_new_runcycleid_passed_to_load_reponse)
+    flag_to_run_step_after_submission = submission_end_run_cycle(submission_new_runcycleid_passed_to_end)
