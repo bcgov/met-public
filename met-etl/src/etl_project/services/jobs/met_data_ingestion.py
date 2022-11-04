73f1f6c64b9e5b582516eb75732e5c4ee033ffbb
VineetBala-AOT <90332175+VineetBala-AOT@users.noreply.github.com>
2022-11-04 16:49:04 -0700

    Dagster etl changes (#812)
    * etls for engagement, survey and user details
    
    * etls for submission and updated job and schedule
    
    * updated etl job
    
    * updated variable names in user etl
    
    * adding comments and formatting
    
    * adding the etl for comments
    
    * fixed an error to handle none
    
    * bug fix
    
    * updated default date

--- met-etl/src/etl_project/services/jobs/met_data_ingestion.py
+++ met-etl/src/etl_project/services/jobs/met_data_ingestion.py
@@ -1,6 +1,46 @@
 from dagster import job
-from ops.user_etl_service import get_user_details_last_run_cycle_time, extract_user, load_new_user
+from ops.user_etl_service import get_user_details_last_run_cycle_time, extract_user, load_user, user_details_end_run_cycle
+from ops.survey_etl_service import get_survey_last_run_cycle_time, extract_survey, load_survey, survey_end_run_cycle
+from ops.submission_etl_service import get_submission_last_run_cycle_time, extract_submission, load_submission, load_user_response_details, submission_end_run_cycle
+from ops.comments_etl_service import get_comments_last_run_cycle_time, extract_comments, load_comments, comments_end_run_cycle
+from ops.engagement_etl_service import extract_engagement, load_engagement
 
 @job
 def met_data_ingestion():
-    load_new_user(extract_user(get_user_details_last_run_cycle_time()))
+    # etl run for survey
+    survey_last_run_cycle_time, survey_new_runcycleid_created = get_survey_last_run_cycle_time()
+    
+    new_survey, updated_survey, survey_new_runcycleid_passed_to_load = extract_survey(survey_last_run_cycle_time, 
+                                                                            survey_new_runcycleid_created)
+
+    survey_new_runcycleid_passed_to_end = load_survey(new_survey, updated_survey, 
+                                                                            survey_new_runcycleid_passed_to_load)
+
+    flag_to_run_step_after_survey = survey_end_run_cycle(survey_new_runcycleid_passed_to_end)
+
+    # etl run for submissions
+    submission_last_run_cycle_time, submission_new_runcycleid_created = get_submission_last_run_cycle_time(
+                                                                            flag_to_run_step_after_survey)
+
+    new_submission, updated_submission, submission_new_runcycleid_passed_to_load = extract_submission(
+                                                                            submission_last_run_cycle_time, 
+                                                                            submission_new_runcycleid_created)
+
+    submission_new_runcycleid_passed_to_load_reponse = load_submission(new_submission, updated_submission, 
+                                                                            submission_new_runcycleid_passed_to_load)
+
+    submission_new_runcycleid_passed_to_end = load_user_response_details(new_submission, updated_submission, 
+                                                                            submission_new_runcycleid_passed_to_load_reponse)
+                                                                        
+    flag_to_run_step_after_submission = submission_end_run_cycle(submission_new_runcycleid_passed_to_end)
+	
+    # etl run for comments
+    comments_last_run_cycle_time, comments_new_runcycleid_created = get_comments_last_run_cycle_time(
+                                                                            flag_to_run_step_after_submission)
+
+    new_comments, comments_new_runcycleid_passed_to_load = extract_comments(comments_last_run_cycle_time, 
+                                                                            comments_new_runcycleid_created)
+
+    comments_new_runcycleid_passed_to_end = load_comments(new_comments, comments_new_runcycleid_passed_to_load)
+    
+    flag_to_run_step_after_comments = comments_end_run_cycle(comments_new_runcycleid_passed_to_end)
---
+++ met-etl/src/etl_project/services/ops/comments_etl_service.py
@@ -0,0 +1,128 @@
+from dagster import Out, Output, op
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
+from utils.config import get_met_db_creds, get_met_analytics_db_creds
+from datetime import datetime
+
+from met_api.models.comment import Comment as MetCommentModel
+from met_api.constants.comment_status import Status as CommentStatus
+from met_cron.models.user_feedback import UserFeedback as UserFeedbackModel
+from met_cron.models.survey import Survey as EtlSurveyModel
+from met_cron.models.etlruncycle import EtlRunCycle as EtlRunCycleModel
+
+def _get_met_session():
+    engine = get_met_db_creds()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+def _get_met_etl_session():
+    engine = get_met_analytics_db_creds()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+# get the last run cycle id for comments etl
+@op(out={"comments_last_run_cycle_datetime": Out(), "comments_new_run_cycle_id": Out()})
+def get_comments_last_run_cycle_time(context, flag_to_trigger_comments_etl):
+    met_etl_db_session = _get_met_etl_session()
+    default_datetime = datetime(1900, 1, 1, 0, 0, 0, 0)
+
+    comments_last_run_cycle_datetime = met_etl_db_session.query(
+        func.coalesce(func.max(EtlRunCycleModel.enddatetime), default_datetime)).filter(
+        EtlRunCycleModel.packagename == 'userfeedback', EtlRunCycleModel.success == True).first()
+
+    max_run_cycle_id = met_etl_db_session.query(func.coalesce(func.max(EtlRunCycleModel.id), 0)).first()
+
+    for last_run_cycle_time in comments_last_run_cycle_datetime:
+
+        for run_cycle_id in max_run_cycle_id:
+
+            new_run_cycle_id = run_cycle_id + 1
+            met_etl_db_session.add(EtlRunCycleModel(id=new_run_cycle_id, packagename='userfeedback', startdatetime=datetime.utcnow(), 
+                        enddatetime=None, description='started the load for table user_feedback', success=False))
+
+            met_etl_db_session.commit()
+
+    met_etl_db_session.close()
+
+    yield Output(comments_last_run_cycle_datetime, "comments_last_run_cycle_datetime")
+
+    yield Output(new_run_cycle_id, "comments_new_run_cycle_id")
+
+# extract the comments that have been created after the last run
+@op(out={"new_comments": Out(), "comments_new_run_cycle_id": Out()})
+def extract_comments(context, comments_last_run_cycle_datetime, comments_new_run_cycle_id):
+    session = _get_met_session()
+    new_comments = []
+
+    for last_run_cycle_time in comments_last_run_cycle_datetime:
+
+        context.log.info("started extracting new data from comments table")
+        new_comments = session.query(MetCommentModel).filter(MetCommentModel.submission_date > last_run_cycle_time, 
+                                                        MetCommentModel.status_id == CommentStatus.Approved.value).all()
+
+    yield Output(new_comments, "new_comments")
+
+    yield Output(comments_new_run_cycle_id, "comments_new_run_cycle_id")
+
+    context.log.info("completed extracting data from comments table")
+
+    session.commit()
+
+    session.close()
+
+# load the comments created after last run to the analytics database
+@op(out={"comments_new_run_cycle_id": Out()})
+def load_comments(context, new_comments, comments_new_run_cycle_id):
+    session = _get_met_etl_session()
+
+    if len(new_comments) > 0:
+
+        context.log.info("loading new comments")
+
+        for comment in new_comments:
+
+            etl_survey = session.query(EtlSurveyModel.id).filter(EtlSurveyModel.source_survey_id == comment.survey_id,
+                                                            EtlSurveyModel.is_active == True).first()
+
+            for survey in etl_survey:
+
+                context.log.info('Survey id in Analytics DB: %s  for Comment: %s with source survey id:%s:', comment.id,
+                            survey, comment.survey_id)
+
+                user_feedback_model = UserFeedbackModel(survey_id = survey,
+                                        user_id = comment.user_id,
+                                        comment = comment.text,
+                                        source_comment_id = comment.id,
+                                        is_active=True, 
+                                        created_date=comment.submission_date,
+                                        updated_date=None,
+                                        runcycle_id=comments_new_run_cycle_id)
+
+                session.add(user_feedback_model)
+
+                session.commit()
+
+    yield Output(comments_new_run_cycle_id, "comments_new_run_cycle_id")
+
+    context.log.info("completed loading comments table")
+
+    session.close()
+
+# update the status for comments etl in run cycle table as successful
+@op(out={"flag_to_run_step_after_comments": Out()})
+def comments_end_run_cycle(context, comments_new_run_cycle_id):
+    met_etl_db_session = _get_met_etl_session()
+
+    met_etl_db_session.query(EtlRunCycleModel).filter(
+        EtlRunCycleModel.id == comments_new_run_cycle_id, EtlRunCycleModel.packagename == 'userfeedback', 
+        EtlRunCycleModel.success == False).update({'success': True, 'description': 'ended the load for table user_feedback'})
+
+    context.log.info("run cycle ended for comments table")
+
+    yield Output("userfeedback", "flag_to_run_step_after_comments")
+
+    met_etl_db_session.commit()
+
+    met_etl_db_session.close()
