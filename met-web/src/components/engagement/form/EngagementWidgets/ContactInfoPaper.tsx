8b388e36a47c04d4454fe816d3eaa78a7958b44a
djnunez-aot <david.nunez@aot-technologies.com>
2022-11-08 11:05:34 -0500

    remove =

--- met-web/src/components/engagement/form/EngagementWidgets/ContactInfoPaper.tsx
+++ met-web/src/components/engagement/form/EngagementWidgets/ContactInfoPaper.tsx
@@ -124,7 +124,6 @@ const ContantInfoPaper = ({ testId, contact, removeContact, index, moveContact, 
                     spacing={1}
                 >
                     <When condition={!!contact.phone_number}>
-                        =
                         <Grid item xs={3}>
                             <MetParagraph>Phone:</MetParagraph>
                         </Grid>
