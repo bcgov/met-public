c09ed907175cea86094ec96c0042499a622bc674
Tiago Graf <60719756+tiagograf-AOT@users.noreply.github.com>
2022-11-18 09:47:29 -0800

    Make nginx.conf dynamic (#869)

---
+++ met-web/nginx/docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE=/etc/nginx/nginx.conf
+MOUNTED_CONFIG_FILE=/nginx.conf
+if [ -f "$MOUNTED_CONFIG_FILE" ]; then
+    echo "Using root configuration file $MOUNTED_CONFIG_FILE."
+    cp $MOUNTED_CONFIG_FILE $CONFIG_FILE
+else 
+    ENV_FILE=/etc/nginx/nginx.$ENV.conf
+    if [ -f "$ENV_FILE" ]; then
+        echo "Root configuration file not found, using environment template $ENV_FILE."
+        cp $ENV_FILE $CONFIG_FILE
+    else 
+        DEFAULT_FILE=/etc/nginx/nginx.dev.conf
+        echo "Root configuration file not found and environment template not found, using default $DEFAULT_FILE."
+        cp $DEFAULT_FILE $CONFIG_FILE
+    fi
+fi
+nginx -g 'daemon off;'
