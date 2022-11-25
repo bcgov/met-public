c5540e8e12a368cf8fc674b6d595a478402ee485
Tiago Graf <tiagograf@gmail.com>
2022-11-25 13:13:25 -0800

    Update store.ts

--- met-web/src/store.ts
+++ met-web/src/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import loginReducer from 'components/Login/loginSlice';
 import userSlice from 'services/userService/userSlice';
 import engagementSlice from 'services/engagementService/engagementSlice';
