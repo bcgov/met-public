2f5b36bca647b3421c36887618e8cf803004823c
djnunez-aot <david.nunez@aot-technologies.com>
2022-12-29 12:13:48 -0500

    update

--- met-web/src/components/engagement/view/SurveyBlock.tsx
+++ met-web/src/components/engagement/view/SurveyBlock.tsx
@@ -7,7 +7,7 @@ import { SurveyBlockProps } from './types';
 import { useAppSelector } from 'hooks';
 
 const SurveyBlock = ({ startSurvey }: SurveyBlockProps) => {
-    const { savedEngagement, isEngagementLoading } = useContext(ActionContext);
+    const { savedEngagement, isEngagementLoading, mockStatus } = useContext(ActionContext);
 
     const isOpen = savedEngagement.submission_status === SubmissionStatus.Open;
     const surveyId = savedEngagement.surveys[0]?.id || '';
@@ -21,25 +21,28 @@ const SurveyBlock = ({ startSurvey }: SurveyBlockProps) => {
     return (
         <MetPaper elevation={1} sx={{ padding: '2em' }}>
             <Grid container direction="row" alignItems="flex-end" justifyContent="flex-end" spacing={2}>
-                <Grid item xs={12}>
-                    <MetHeader1>Let us know what you think!</MetHeader1>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">
-                        We would like to hear from you about this project and how it could affect you and your
-                        environment. Please fill in our short survey and leave a comment. Survey takes 3-5min to
-                        complete
-                    </Typography>
-                </Grid>
-                <Grid item container direction={{ xs: 'column', sm: 'row' }} xs={12} justifyContent="flex-end">
-                    <PrimaryButton
-                        data-testid="SurveyBlock/take-me-to-survey-button"
-                        disabled={!surveyId || (!isOpen && !isPreview)}
-                        onClick={startSurvey}
-                    >
-                        Take me to the survey
-                    </PrimaryButton>
-                </Grid>
+                <When condition={{ isPreview }}></When>
+                <Else>
+                    <Grid item xs={12}>
+                        <MetHeader1>Let us know what you think!</MetHeader1>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1">
+                            We would like to hear from you about this project and how it could affect you and your
+                            environment. Please fill in our short survey and leave a comment. Survey takes 3-5min to
+                            complete
+                        </Typography>
+                    </Grid>
+                    <Grid item container direction={{ xs: 'column', sm: 'row' }} xs={12} justifyContent="flex-end">
+                        <PrimaryButton
+                            data-testid="SurveyBlock/take-me-to-survey-button"
+                            disabled={!surveyId || (!isOpen && !isPreview)}
+                            onClick={startSurvey}
+                        >
+                            Take me to the survey
+                        </PrimaryButton>
+                    </Grid>
+                </Else>
             </Grid>
         </MetPaper>
     );
