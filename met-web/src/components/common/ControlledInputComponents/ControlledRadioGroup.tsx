b256718d54b89a1af315dbcd273caaab2ba9f27b
Jad <jad.saad@aot-technologies.com>
2023-01-09 12:35:07 -0800

    Add controlled radio and autocomplete

---
+++ met-web/src/components/common/ControlledInputComponents/ControlledRadioGroup.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { RadioGroup, RadioGroupProps } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+
+type IFormInputProps = {
+    name: string;
+} & RadioGroupProps;
+
+const ControlledRadiGroup: FC<IFormInputProps> = ({ name, children, ...otherProps }) => {
+    const {
+        control,
+        formState: { errors, defaultValues },
+    } = useFormContext();
+
+    return (
+        <Controller
+            control={control}
+            name={name}
+            defaultValue={defaultValues?.[name] || ''}
+            render={({ field }) => (
+                <RadioGroup
+                    {...otherProps}
+                    {...field}
+                    // error={!!errors[name]}
+                    // helperText={String(errors[name]?.message || '')}
+                >
+                    {children}
+                </RadioGroup>
+            )}
+        />
+    );
+};
+
+export default ControlledRadiGroup;
--- met-web/src/components/userManagement/listing/AddUserModal.tsx
+++ met-web/src/components/userManagement/listing/AddUserModal.tsx
@@ -1,16 +1,14 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { useAppDispatch } from 'hooks';
 import Modal from '@mui/material/Modal';
 import {
     Autocomplete,
-    Box,
     FormControl,
     FormControlLabel,
     FormLabel,
     Grid,
     Paper,
     Radio,
-    RadioGroup,
     Stack,
     TextField,
 } from '@mui/material';
@@ -18,106 +16,129 @@ import { MetHeader3, MetLabel, modalStyle, PrimaryButton, SecondaryButton } from
 import { User } from 'models/user';
 import { UserManagementContext } from './UserManagementContext';
 import { Palette } from 'styles/Theme';
-import { useForm, FormProvider, SubmitHandler } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import ControlledRadiGroup from 'components/common/ControlledInputComponents/ControlledRadioGroup';
 
 const schema = yup
     .object({
-        name: yup.string(),
-        group: yup.string(),
+        group: yup.string().required(),
+        user: yup.object().required(),
     })
     .required();
 
 type AddUserForm = yup.TypeOf<typeof schema>;
 
 export const AddUserModel = () => {
-    const dispatch = useAppDispatch();
+    // const dispatch = useAppDispatch();
     const { addUserModalOpen, setAddUserModelOpen, users } = useContext(UserManagementContext);
-    const [availableUsers, setAvailableUsers] = useState<User[]>([]);
-    const [selectedUser, setSelectedUser] = useState<User | null>(null);
 
     const methods = useForm<AddUserForm>({
         resolver: yupResolver(schema),
-        defaultValues: {
-            name: '',
-            group: '',
-        },
     });
 
-    const { handleSubmit, register } = methods;
+    const { handleSubmit, control, reset } = methods;
 
     const handleClose = () => {
         setAddUserModelOpen(false);
+        reset({});
     };
 
     const onSubmit: SubmitHandler<AddUserForm> = async (data: AddUserForm) => {
-        setAddUserModelOpen(false);
-        console.log(data);
+        handleClose();
     };
 
     return (
-        <Modal open={addUserModalOpen} onClose={handleClose}>
+        <Modal open={addUserModalOpen} onClose={handleClose} keepMounted={false}>
             <Paper sx={{ ...modalStyle }}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <Grid container direction="row" alignItems="flex-start" justifyContent="flex-start" spacing={2}>
-                        <Grid item xs={12}>
-                            <MetHeader3 bold>Add a User</MetHeader3>
-                        </Grid>
+                <FormProvider {...methods}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <Grid container direction="row" alignItems="flex-start" justifyContent="flex-start" spacing={2}>
+                            <Grid item xs={12}>
+                                <MetHeader3 bold>Add a User</MetHeader3>
+                            </Grid>
 
-                        <Grid item xs={12}>
-                            <MetLabel sx={{ marginBottom: '2px', display: 'flex' }}>
-                                Select the user you want to add
-                            </MetLabel>
-
-                            <Autocomplete
-                                id="engagement-selector"
-                                options={users || []}
-                                renderInput={(params) => (
-                                    <TextField {...params} fullWidth placeholder="(Select one)" name="name" />
-                                )}
-                                size="small"
-                                getOptionLabel={(user: User) => `${user.first_name} ${user.last_name}`}
-                                disabled={false}
-                                {...register('name')}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <FormControl>
-                                <FormLabel
-                                    id="controlled-radio-buttons-group"
-                                    sx={{ fontWeight: 'bold', color: Palette.text.primary }}
-                                >
-                                    What role would you like to assign to this user?
-                                </FormLabel>
-                                <RadioGroup aria-labelledby="controlled-radio-buttons-group" {...register('group')}>
-                                    <FormControlLabel value="admin" control={<Radio />} label="Adminstrator" />
-                                    <FormControlLabel value="teamMember" control={<Radio />} label="Team Member" />
-                                </RadioGroup>
-                            </FormControl>
-                        </Grid>
-
-                        <Grid
-                            item
-                            container
-                            xs={12}
-                            direction="row"
-                            justifyContent="flex-end"
-                            spacing={1}
-                            sx={{ mt: '1em' }}
-                        >
-                            <Stack
-                                direction={{ md: 'column-reverse', lg: 'row' }}
-                                spacing={1}
-                                width="100%"
-                                justifyContent="flex-end"
+                            <Grid
+                                item
+                                xs={12}
+                                container
+                                direction="row"
+                                alignItems="flex-start"
+                                justifyContent="flex-start"
+                                rowSpacing={4}
                             >
-                                <SecondaryButton onClick={handleClose}>Cancel</SecondaryButton>
-                                <PrimaryButton type="submit">Submit</PrimaryButton>
-                            </Stack>
+                                <Grid item xs={12}>
+                                    <MetLabel sx={{ marginBottom: '2px', display: 'flex' }}>
+                                        Select the user you want to add
+                                    </MetLabel>
+
+                                    <Controller
+                                        control={control}
+                                        name="user"
+                                        render={({ field: { ref, onChange, ...field } }) => (
+                                            <Autocomplete
+                                                options={users || []}
+                                                onChange={(_, data) => {
+                                                    onChange(data);
+                                                }}
+                                                renderInput={(params) => (
+                                                    <TextField
+                                                        {...params}
+                                                        {...field}
+                                                        inputRef={ref}
+                                                        fullWidth
+                                                        placeholder="(Select one)"
+                                                    />
+                                                )}
+                                                getOptionLabel={(user: User) => `${user.first_name} ${user.last_name}`}
+                                            />
+                                        )}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <FormControl>
+                                        <FormLabel
+                                            id="controlled-radio-buttons-group"
+                                            sx={{ fontWeight: 'bold', color: Palette.text.primary }}
+                                        >
+                                            What role would you like to assign to this user?
+                                        </FormLabel>
+                                        <ControlledRadiGroup name="group">
+                                            <FormControlLabel value="admin" control={<Radio />} label="Adminstrator" />
+                                            <FormControlLabel
+                                                value="teamMember"
+                                                control={<Radio />}
+                                                label="Team Member"
+                                                disabled
+                                            />
+                                        </ControlledRadiGroup>
+                                    </FormControl>
+                                </Grid>
+                            </Grid>
+
+                            <Grid
+                                item
+                                container
+                                xs={12}
+                                direction="row"
+                                justifyContent="flex-end"
+                                spacing={1}
+                                sx={{ mt: '1em' }}
+                            >
+                                <Stack
+                                    direction={{ md: 'column-reverse', lg: 'row' }}
+                                    spacing={1}
+                                    width="100%"
+                                    justifyContent="flex-end"
+                                >
+                                    <SecondaryButton onClick={handleClose}>Cancel</SecondaryButton>
+                                    <PrimaryButton type="submit">Submit</PrimaryButton>
+                                </Stack>
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </form>
+                    </form>
+                </FormProvider>
             </Paper>
         </Modal>
     );
