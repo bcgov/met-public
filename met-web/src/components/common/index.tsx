b7c6282220574320c4d5f7e8eb129568a34ac8a6
djnunez-aot <david.nunez@aot-technologies.com>
2023-01-17 13:47:11 -0500

    move max height and min height

--- met-web/src/components/common/index.tsx
+++ met-web/src/components/common/index.tsx
@@ -31,8 +31,6 @@ export const StyledPrimaryButton = styled(LoadingButton)(() => ({
         backgroundColor: Palette.primary.main,
         color: '#fff',
         textDecoration: 'underline',
-        minHeight: '40px',
-        maxHeight: '40px',
     },
 }));
 
@@ -46,8 +44,6 @@ export const StyledSecondaryButton = styled(MuiButton)(() => ({
         backgroundColor: Palette.primary.main,
         color: '#FFFFFF',
         border: `2px solid ${Palette.primary.main}`,
-        minHeight: '40px',
-        maxHeight: '40px',
     },
 }));
 
@@ -61,19 +57,17 @@ export const StyledWidgetButton = styled(MuiButton)(() => ({
         backgroundColor: '#f2f2f2',
         color: '#494949',
         border: `2px solid ${'#f2f2f2'}`,
-        minHeight: '40px',
-        maxHeight: '40px',
     },
 }));
 
 export const WidgetButton = ({ children, ...rest }: { children: React.ReactNode; [prop: string]: unknown }) => (
-    <StyledWidgetButton {...rest} variant="outlined">
+    <StyledWidgetButton {...rest} sx={{ minHeight: '40px', maxHeight: '40px' }} variant="outlined">
         {children}
     </StyledWidgetButton>
 );
 
 export const SecondaryButton = ({ children, ...rest }: { children: React.ReactNode; [prop: string]: unknown }) => (
-    <StyledSecondaryButton {...rest} variant="outlined">
+    <StyledSecondaryButton {...rest} sx={{ minHeight: '40px', maxHeight: '40px' }} variant="outlined">
         {children}
     </StyledSecondaryButton>
 );
@@ -82,6 +76,7 @@ export const PrimaryButton = ({ children, ...rest }: { children: React.ReactNode
     <StyledPrimaryButton
         {...rest}
         variant="contained"
+        sx={{ minHeight: '40px', maxHeight: '40px' }}
         loadingIndicator={<CircularProgress color="primary" size={'1.8em'} />}
     >
         {children}
