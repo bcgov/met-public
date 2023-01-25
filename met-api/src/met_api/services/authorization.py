390bdfcae1f3efa7a6e8da9497342d218bcf977b
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2023-01-25 12:20:09 -0800

    membership add and edit (#1143)
    * membership add and edit
    
    * Removed propage exceptions

---
+++ met-api/src/met_api/services/authorization.py
@@ -0,0 +1,10 @@
+"""The Authorization service.
+
+This module is to handle authorization related queries.
+"""
+
+
+# pylint: disable=unused-argument
+def check_auth(**kwargs):
+    """Check if user is authorized to perform action on the service."""
+    return True
