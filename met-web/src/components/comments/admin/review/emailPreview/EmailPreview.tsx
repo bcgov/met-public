0d7aa55019d4ae52c5858f7758a81c23e080dd4a
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2023-01-31 18:24:20 -0400

    Update email template (#1178)
    * Preview Button styling update
    
    * update email template
    
    * remove duplicate imports

--- met-web/src/components/comments/admin/review/emailPreview/EmailPreview.tsx
+++ met-web/src/components/comments/admin/review/emailPreview/EmailPreview.tsx
@@ -2,16 +2,19 @@ import { Box, Grid, Stack } from '@mui/material';
 import * as React from 'react';
 import { MetHeader4, MetBody } from 'components/common';
 import { ReactComponent as BCLogo } from 'assets/images/BritishColumbiaLogoLight.svg';
+import { Survey } from 'models/survey';
+import { formatDate } from 'components/common/dateHelper';
 
 export default function EmailPreview({
-    engagement_name,
+    survey,
     children,
     ...rest
 }: {
-    engagement_name: string;
+    survey: Survey;
     children: React.ReactNode;
     [prop: string]: unknown;
 }) {
+    const scheduledDate = formatDate(survey.engagement?.scheduled_date || '', 'MMM DD YYYY');
     return (
         <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             <Box style={container}>
@@ -27,11 +30,18 @@ export default function EmailPreview({
                     />
                 </Stack>
                 <Grid item xs={12}>
-                    <MetHeader4 sx={{ mb: 1 }}>
-                        Thank you for taking the time to fill in our survey about {engagement_name}.
+                    <MetHeader4 bold sx={{ mb: 1 }}>
+                        Thank you for taking the time to fill in our survey about {survey.engagement?.name} -{' '}
+                        {survey.name}.
                     </MetHeader4>
                 </Grid>
                 {children}
+                <Grid item xs={12}>
+                    <MetHeader4 bold sx={{ mb: 1 }}>
+                        The engagement period is open until
+                        {scheduledDate}.
+                    </MetHeader4>
+                </Grid>
                 <Grid item xs={12}>
                     <MetBody sx={{ mb: 1 }}>Thank you,</MetBody>
                 </Grid>
