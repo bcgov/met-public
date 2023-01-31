97b58482a58dc3b0f90984f41514f14c615e9b34
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2023-01-31 19:54:00 -0400

    Preview Modal tweaks (#1180)
    * Preview Button styling update
    
    * update email template
    
    * remove duplicate imports
    
    * update text
    
    * add of
    
    * add brackets

--- met-web/src/components/comments/admin/review/emailPreview/EmailTemplates.tsx
+++ met-web/src/components/comments/admin/review/emailPreview/EmailTemplates.tsx
@@ -51,7 +51,7 @@ export const RejectEmailTemplate = ({
                     <Grid item xs={12}>
                         <MetBody
                             sx={{ mb: 1 }}
-                        >{`One or many of your comments can't be published because ${otherReason}.`}</MetBody>
+                        >{`One or many of your comments can't be published because of (${otherReason}).`}</MetBody>
                     </Grid>
                 </li>
             </When>
