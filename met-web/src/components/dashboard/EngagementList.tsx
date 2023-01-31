ac7aec727feb7f66b2027998ae8a7e77d3588334
Tiago Graf <60719756+tiagograf-AOT@users.noreply.github.com>
2023-01-31 14:59:33 -0300

    Update EngagementList.tsx (#1175)

--- met-web/src/components/dashboard/EngagementList.tsx
+++ met-web/src/components/dashboard/EngagementList.tsx
@@ -25,8 +25,9 @@ const EngagementList = () => {
     );
 
     const StyledAccordion = styled(Accordion)({
-        border: 'solid 1px #606060',
-        boxShadow: 'none',
+        border: 'solid 1px #cdcdcd',
+        boxShadow:
+            'rgb(0 0 0 / 6%) 0px 2px 2px -1px, rgb(0 0 0 / 6%) 0px 1px 1px 0px, rgb(0 0 0 / 6%) 0px 1px 3px 0px;',
     });
 
     if (isLoading) {
