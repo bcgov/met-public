1b19cf3f033cc610f917e7a6a45fb00b97396bd3
Jad <jad.saad@aot-technologies.com>
2023-02-03 16:52:39 -0800

    Add current phase text under title

--- met-web/src/components/engagement/view/widgets/PhasesWidget/index.tsx
+++ met-web/src/components/engagement/view/widgets/PhasesWidget/index.tsx
@@ -1,12 +1,13 @@
 import React, { useContext, useState } from 'react';
 import { Grid, Skeleton, Stack, Box } from '@mui/material';
-import { MetBody, MetHeader3, MetPaper, MetSmallText } from 'components/common';
+import { MetBody, MetHeader3, MetHeader4, MetLabel, MetPaper, MetParagraph, MetSmallText } from 'components/common';
 import { WidgetType } from 'models/widget';
 import { ActionContext } from '../../ActionContext';
 import { EngagementPhase } from './EngagementPhase';
-import { ENGAGEMENT_PHASES } from 'models/engagementPhases';
+import { EngagementPhases, ENGAGEMENT_PHASES } from 'models/engagementPhases';
 import { ForumIcon } from './ForumIcon';
 import { InfoArrow } from './InfoArrow';
+import { When } from 'react-if';
 
 interface PhaseContextProps {
     anchorEl: HTMLButtonElement | null;
@@ -24,6 +25,7 @@ export const PhasesWidget = () => {
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
     const phases = Object.values(ENGAGEMENT_PHASES);
     const phasesWidget = widgets.find((widget) => widget.widget_type_id === WidgetType.Phases);
+    const currentPhaseId = phasesWidget?.items[0]?.widget_data_id || EngagementPhases.Standalone;
 
     if (isWidgetsLoading) {
         return <Skeleton variant="rectangular" height={'20em'} />;
@@ -41,10 +43,18 @@ export const PhasesWidget = () => {
             }}
         >
             <MetPaper elevation={1} sx={{ padding: '2em' }}>
-                <Grid container direction="row" justifyContent="flex-start" alignItems="flex-start" rowSpacing={2}>
+                <Grid container direction="row" justifyContent="flex-start" alignItems="flex-start">
                     <Grid item xs={12}>
                         <MetHeader3 bold>The EA Process</MetHeader3>
                     </Grid>
+
+                    <When condition={Boolean(currentPhaseId)}>
+                        <Grid xs={12}>
+                            <MetLabel>
+                                Current Phase: {phases.find((phase) => phase.phaseId === currentPhaseId)?.title || ''}
+                            </MetLabel>
+                        </Grid>
+                    </When>
                     <Grid item xs={12}>
                         <MetBody>
                             Click on the sections below to expand them and learn more about each EA process phase. You
