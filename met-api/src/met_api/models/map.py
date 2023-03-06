0cd6bd522e84a067783ccd8bfecfd0d8f9c148cf
djnunez-aot <david.nunez@aot-technologies.com>
2023-03-06 15:35:35 -0500

    move create map function from model to widget_map_service

--- met-api/src/met_api/models/map.py
+++ met-api/src/met_api/models/map.py
@@ -3,9 +3,10 @@
 Manages the map widget
 """
 from __future__ import annotations
-from datetime import datetime
+
 from sqlalchemy.dialects.postgresql import JSON
 from sqlalchemy.sql.schema import ForeignKey
+
 from .base_model import BaseModel
 from .db import db
 
@@ -20,30 +21,11 @@ class WidgetMap(BaseModel):  # pylint: disable=too-few-public-methods
     title = db.Column(db.String(50))
     latitutde = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
-    shapefile = db.Column(JSON, unique=False, nullable=True)
 
     @classmethod
     def get_map(cls, widget_id) -> list[WidgetMap]:
-        """Get map"""
+        """Get map."""
         return db.session.query(WidgetMap).filter_by(WidgetMap.widget_id == widget_id)
-
-    @classmethod
-    def create_map(cls, map_data) -> WidgetMap:
-        """Create map."""
-        new_map = WidgetMap(
-            title=map_data.get('title', None),
-            latitude=map_data.get('latitude', None),
-            longitude=map_data.get('longitude', None),
-            shapefile=map_data.get('shapefile', None),
-            created_date=datetime.utcnow(),
-            updated_date=datetime.utcnow(),
-            created_by=map_data.get('created_by', None),
-            updated_by=map_data.get('updated_by', None),
-        )
-        db.session.add(new_map)
-        db.session.commit()
-
-        return new_map
 
     @classmethod
     def update_map(cls, widget_id, map_data: dict) -> WidgetMap:
--- met-api/src/met_api/services/widget_map_service.py
+++ met-api/src/met_api/services/widget_map_service.py
@@ -1,10 +1,26 @@
 """Service for Widget Map management."""
 from http import HTTPStatus
+from datetime import datetime
 from met_api.exceptions.business_exception import BusinessException
 from met_api.models.widget_map import WidgetMap as WidgetMapModel
 
 class WidgetMapService:
     """Widget Map management service."""
+    
+    @staticmethod
+    def _create_map_model(widget_id, map_data: dict):
+        map = WidgetMapModel()
+        map.widget_id = widget_id
+        map.title = map_data.get('title')
+        map.latitude = map_data.get('latitude')
+        map.title = map_data.get('longitude')
+        map.engagement_id = map_data.get('engagement_id')
+        map.created_date=datetime.utcnow(),
+        map.updated_date=datetime.utcnow(),
+        map.created_by=map_data.get('created_by', None),
+        map.updated_by=map_data.get('updated_by', None),
+        map.flush()
+        return map
 
     @staticmethod
     def _create_map_model(widget_id, map_data: dict):
@@ -39,3 +55,5 @@ class WidgetMapService:
                 error='Invalid widgets and map',
                 status_code=HTTPStatus.BAD_REQUEST)
         return WidgetMapModel.update_map(widget_id, request_json)
+    
+
