956253655485f50a997685891448ba06a0027f91
djnunez-aot <david.nunez@aot-technologies.com>
2023-03-06 15:35:35 -0500

    move create map function from model to widget_map_service

--- met-api/src/met_api/services/widget_map_service.py
+++ met-api/src/met_api/services/widget_map_service.py
@@ -1,10 +1,26 @@
 """Service for Widget Map management."""
 from http import HTTPStatus
+from datetime import datetime
 from met_api.exceptions.business_exception import BusinessException
 from met_api.models.map import WidgetMap as WidgetMapModel
 
 class WidgetMapService:
     """Widget Map management service."""
+    
+    @staticmethod
+    def _create_map_model(widget_id, map_data: dict):
+        map = WidgetMapModel()
+        map.widget_id = widget_id
+        map.title = map_data.get('title')
+        map.latitude = map_data.get('latitude')
+        map.title = map_data.get('longitude')
+        map.engagement_id = map_data.get('engagement_id')
+        map.created_date=datetime.utcnow(),
+        map.updated_date=datetime.utcnow(),
+        map.created_by=map_data.get('created_by', None),
+        map.updated_by=map_data.get('updated_by', None),
+        map.flush()
+        return map
 
     @staticmethod
     def get_map(widget_id):
@@ -13,9 +29,10 @@ class WidgetMapService:
         return widget_map
 
     @staticmethod
-    def create_map(map_details: dict):
+    def create_map(widget_id, map_details: dict):
         """Create map for the widget."""
-        widget_map = WidgetMapModel.create_map(map_details)
+        widget_map = WidgetMapModel._create_map_model(widget_id, map_details)
+        widget_map.commit()
         return widget_map
 
     @staticmethod
@@ -27,3 +44,5 @@ class WidgetMapService:
                 error='Invalid widgets and map',
                 status_code=HTTPStatus.BAD_REQUEST)
         return WidgetMapModel.update_map(widget_id, request_json)
+    
+
