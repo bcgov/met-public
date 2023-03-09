0880b685d83fba300336be9ec9dfa5dd8a11d34a
Jad <jad.saad@aot-technologies.com>
2023-03-09 13:12:07 -0800

    Add a dummy parameter to image src url

--- met-web/src/components/imageUpload/cropImage.tsx
+++ met-web/src/components/imageUpload/cropImage.tsx
@@ -4,8 +4,8 @@ export const createImage = (url: string) =>
         const image = new Image();
         image.addEventListener('load', () => resolve(image));
         image.addEventListener('error', (error) => reject(error));
-        image.setAttribute('crossOrigin', 'anonymous'); // needed to avoid cross-origin issues on CodeSandbox
-        image.src = url;
+        image.setAttribute('crossOrigin', ''); // needed to avoid cross-origin issues on CodeSandbox
+        image.src = url + '?not-from-cache';
     });
 
 export function getRadianAngle(degreeValue: number) {
