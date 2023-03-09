d822be63e780097113ef60179ccc89565143cb12
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2023-03-09 15:11:30 -0500

    Map widget model  (#1324)
    * create map model
    
    * map widget routes & model
    
    * code smell fix
    
    * update widget map model name
    
    * update schema and add engagement id
    
    * schema update & code smells
    
    * remove self assignment
    
    * api lint
    
    * update back end & lint fix
    
    * update parameters
    
    * remove ununsed variable
    
    * move create map function from model to widget_map_service
    
    * update map to map_model
    
    * lint
    
    * add spacing
    
    * rename map to widget_map
    
    * change title to description
    
    * update api routes
    
    * update
    
    * lint
    
    * Migration
    
    * flake 8 lint
    
    * cleanup
    
    * remove unecessary initialization
    
    * remove import
    
    * code smell
    
    * remove unecessary upgrade and downgrade functions
    
    * add merge back

---
+++ met-api/src/met_api/models/widget_map.py
@@ -0,0 +1,38 @@
+"""WidgetMap model class.
+
+Manages the map widget
+"""
+from __future__ import annotations
+
+from sqlalchemy.sql.schema import ForeignKey
+
+from .base_model import BaseModel
+from .db import db
+
+
+class WidgetMap(BaseModel):  # pylint: disable=too-few-public-methods, too-many-instance-attributes
+    """Definition of the Map entity."""
+
+    __tablename__ = 'widget_map'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    widget_id = db.Column(db.Integer, ForeignKey('widget.id', ondelete='CASCADE'), nullable=True)
+    engagement_id = db.Column(db.Integer, ForeignKey('engagement.id', ondelete='CASCADE'), nullable=True)
+    description = db.Column(db.String(500))
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    @classmethod
+    def get_map(cls, widget_id) -> list[WidgetMap]:
+        """Get map."""
+        return db.session.query(WidgetMap).filter_by(WidgetMap.widget_id == widget_id)
+
+    @classmethod
+    def update_map(cls, widget_id, map_data: dict) -> WidgetMap:
+        """Update map."""
+        query = WidgetMap.query.filter_by(WidgetMap.widget_id == widget_id)
+        widget_map: WidgetMap = query.first()
+        if not widget_map:
+            return map_data
+        query.update(map_data)
+        db.session.commit()
+        return widget_map
