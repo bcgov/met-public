7340c03c8148ef49950bbe9452033525520350c7
djnunez-aot <david.nunez@aot-technologies.com>
2023-03-23 15:45:38 -0400

    update type

--- met-web/src/components/engagement/form/EngagementWidgets/Map/utils.tsx
+++ met-web/src/components/engagement/form/EngagementWidgets/Map/utils.tsx
@@ -1,6 +1,6 @@
 import { GeoJSON } from 'geojson';
 
 export const geoJSONDecode = (geojson_string: string | undefined) => {
-    const geojson: GeoJSON | undefined = geojson_string ? JSON.parse(geojson_string.replace(/\\/g, '')) : undefined;
+    const geojson: GeoJSON = geojson_string ? JSON.parse(geojson_string.replace(/\\/g, '')) : undefined;
     return geojson;
 };
