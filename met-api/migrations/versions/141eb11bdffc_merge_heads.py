8d27acd328817d4c77ea06c1bbdcf07f01c3d4ef
djnunez-aot <david.nunez@aot-technologies.com>
2023-03-27 15:03:01 -0400

    deal with merge conflict

---
+++ met-api/migrations/versions/141eb11bdffc_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 141eb11bdffc
+Revises: e3427bc518f5, f99eb7f53041
+Create Date: 2023-03-27 15:02:24.649423
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '141eb11bdffc'
+down_revision = ('e3427bc518f5', 'f99eb7f53041')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
