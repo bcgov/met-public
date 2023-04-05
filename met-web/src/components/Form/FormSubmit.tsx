6e4e0ed33c2fa97777a25f3ddc013f2e23fa2593
djnunez-aot <david.nunez@aot-technologies.com>
2023-04-05 14:10:28 -0400

    remove unused

--- met-web/src/components/Form/FormSubmit.tsx
+++ met-web/src/components/Form/FormSubmit.tsx
@@ -1,25 +1,34 @@
 /* eslint-disable */
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Form } from '@formio/react';
 import { FormSubmissionData, FormSubmitterProps } from './types';
 import ProgressBar from 'components/survey/submit/FormProgressBar';
+
+interface PageData {
+    page: number;
+    submission: unknown;
+}
 
 const FormSubmit = ({ handleFormChange, savedForm, handleFormSubmit }: FormSubmitterProps) => {
     const [currentPage, setCurrentPage] = useState(1);
 
     return (
         <div className="formio">
-            <ProgressBar
-                currentPage={currentPage}
-                totalPages={
-                    savedForm != undefined ? (savedForm.display === 'wizard' ? savedForm.components.length : 1) : 1
-                }
-            />
+            {savedForm && savedForm.display === 'wizard' ? (
+                <ProgressBar
+                    currentPage={currentPage}
+                    totalPages={
+                        savedForm != undefined ? (savedForm.display === 'wizard' ? savedForm.components.length : 1) : 1
+                    }
+                />
+            ) : (
+                <></>
+            )}
             <Form
                 form={savedForm || { display: 'form' }}
                 onChange={(form: unknown) => handleFormChange(form as FormSubmissionData)}
-                onNextPage={(pageData) => setCurrentPage(pageData.page + 1)}
-                onPrevPage={(pageData) => setCurrentPage(pageData.page + 1)}
+                onNextPage={(pageData: PageData) => setCurrentPage(pageData.page + 1)}
+                onPrevPage={(pageData: PageData) => setCurrentPage(pageData.page + 1)}
                 onSubmit={(form: unknown) => {
                     const formSubmissionData = form as FormSubmissionData;
                     handleFormSubmit(formSubmissionData.data);
--- met-web/src/components/survey/submit/FormProgressBar.tsx
+++ met-web/src/components/survey/submit/FormProgressBar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 export interface ProgressBarProps {
@@ -10,8 +9,8 @@ export interface ProgressBarProps {
 
 function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
     return (
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Box sx={{ width: '100%', mr: 1 }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', pb: 2 }}>
+            <Box sx={{ width: '100%', mr: 1, p: 3 }}>
                 <LinearProgress variant="determinate" {...props} />
             </Box>
         </Box>
