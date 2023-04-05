6d4112a9bcfd347af6baa5576878538b5a65ac23
djnunez-aot <david.nunez@aot-technologies.com>
2023-04-05 13:22:36 -0400

    remove %

--- met-web/src/components/survey/submit/FormProgressBar.tsx
+++ met-web/src/components/survey/submit/FormProgressBar.tsx
@@ -14,9 +14,6 @@ function LinearProgressWithLabel(props: LinearProgressProps & { value: number })
             <Box sx={{ width: '100%', mr: 1 }}>
                 <LinearProgress variant="determinate" {...props} />
             </Box>
-            <Box sx={{ minWidth: 35 }}>
-                <Typography variant="body2" color="text.secondary">{`${Math.round(props.value)}%`}</Typography>
-            </Box>
         </Box>
     );
 }
