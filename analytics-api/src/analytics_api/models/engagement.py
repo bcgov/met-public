06cbb92267c3a365fec8363d0fd2add23dad7343
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2023-05-01 14:29:37 -0700

    analytics db changes

--- analytics-api/src/analytics_api/models/engagement.py
+++ analytics-api/src/analytics_api/models/engagement.py
@@ -3,6 +3,7 @@
 Manages the engagement
 """
 from datetime import datetime
+from sqlalchemy.sql.expression import true
 
 
 from .base_model import BaseModel
@@ -20,6 +21,10 @@ class Engagement(BaseModel):  # pylint: disable=too-few-public-methods
     start_date = db.Column(db.DateTime, default=datetime.utcnow)
     end_date = db.Column(db.DateTime, onupdate=datetime.utcnow)
     published_date = db.Column(db.DateTime, onupdate=datetime.utcnow)
+    latitude = db.Column(db.Float)
+    longitude = db.Column(db.Float)
+    geojson = db.Column(db.Text())
+    marker_label = db.Column(db.String(30))
 
     @classmethod
     def find_by_source_id(cls, source_identifier: int):
@@ -31,3 +36,16 @@ class Engagement(BaseModel):  # pylint: disable=too-few-public-methods
         """Deactivate model by source_identifier."""
         return cls.query.filter_by(source_engagement_id=source_identifier).update(
             {Engagement.is_active: False})
+    
+    @classmethod
+    def get_engagement_map_data(
+        cls,
+        engagement_id
+    ):
+        """Get active engagement record for an engagement id."""
+        map_data = (db.session.query(Engagement.geojson, Engagement.latitude,
+                                     Engagement.longitude, Engagement.marker_label)
+                      .filter(Engagement.source_engagement_id == engagement_id)
+                      .filter(Engagement.is_active == true()))
+
+        return map_data.first()
