cfdc089f5696a01f4329f7b438eb127aa97df437
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2023-05-04 15:48:31 -0700

    fixing lint issues

--- analytics-api/src/analytics_api/models/request_type_option.py
+++ analytics-api/src/analytics_api/models/request_type_option.py
@@ -2,13 +2,13 @@
 
 Manages the option type questions on a survey
 """
+from sqlalchemy import func
+from sqlalchemy.sql.expression import true
+from analytics_api.models.survey import Survey as SurveyModel
+from analytics_api.models.response_type_option import ResponseTypeOption as ResponseTypeOptionModel
 from .base_model import BaseModel
 from .db import db
 from .request_mixin import RequestMixin
-from analytics_api.models.survey import Survey as SurveyModel
-from analytics_api.models.response_type_option import ResponseTypeOption as ResponseTypeOptionModel
-from sqlalchemy import func
-from sqlalchemy.sql.expression import true
 
 
 class RequestTypeOption(BaseModel, RequestMixin):  # pylint: disable=too-few-public-methods
@@ -23,33 +23,31 @@ class RequestTypeOption(BaseModel, RequestMixin):  # pylint: disable=too-few-pub
     ):
         """Get survey result for an engagement id."""
         analytics_survey_id = (db.session.query(SurveyModel.id)
-                    .filter(SurveyModel.engagement_id == engagement_id)
-                    .filter(SurveyModel.is_active == true())
-                    .subquery())
+                               .filter(SurveyModel.engagement_id == engagement_id)
+                               .filter(SurveyModel.is_active == true())
+                               .subquery())
 
         survey_question = (db.session.query(RequestTypeOption.postion.label('postion'),
                                             RequestTypeOption.label.label('label'),
                                             RequestTypeOption.request_id)
-                                            .filter(RequestTypeOption.survey_id.in_(analytics_survey_id))
-                                            .filter(RequestTypeOption.is_active == true())
-                                            .order_by(RequestTypeOption.postion)
-                                            .subquery())
+                           .filter(RequestTypeOption.survey_id.in_(analytics_survey_id))
+                           .filter(RequestTypeOption.is_active == true())
+                           .order_by(RequestTypeOption.postion)
+                           .subquery())
 
         survey_response = (db.session.query(ResponseTypeOptionModel.request_id, ResponseTypeOptionModel.value,
                                             func.count(ResponseTypeOptionModel.request_id).label('response'))
-                                            .filter(ResponseTypeOptionModel.survey_id.in_(analytics_survey_id))
-                                            .filter(ResponseTypeOptionModel.is_active == true())
-                                            .group_by(ResponseTypeOptionModel.request_id,
-                                                      ResponseTypeOptionModel.value)
-                                            .subquery())
+                           .filter(ResponseTypeOptionModel.survey_id.in_(analytics_survey_id))
+                           .filter(ResponseTypeOptionModel.is_active == true())
+                           .group_by(ResponseTypeOptionModel.request_id, ResponseTypeOptionModel.value)
+                           .subquery())
 
         survey_result = (db.session.query((survey_question.c.postion).label('postion'),
                                           (survey_question.c.label).label('question'),
-                                         func.json_agg(func.json_build_object('value', survey_response.c.value,
-                                                                             'count', survey_response.c.response))
-                                                                             .label('result'))
-                                         .join(survey_response,
-                                               survey_response.c.request_id == survey_question.c.request_id)
-                                         .group_by(survey_question.c.postion, survey_question.c.label))
+                                          func.json_agg(func.json_build_object('value', survey_response.c.value,
+                                                                               'count', survey_response.c.response))
+                                          .label('result'))
+                         .join(survey_response, survey_response.c.request_id == survey_question.c.request_id)
+                         .group_by(survey_question.c.postion, survey_question.c.label))
 
         return survey_result.all()
