bf5f114be56a9fd74009577830d16a84200663c8
djnunez-aot <david.nunez@aot-technologies.com>
2023-05-05 11:26:10 -0400

    user details page

--- met-web/src/components/userManagement/userDetails/UserDetails.tsx
+++ met-web/src/components/userManagement/userDetails/UserDetails.tsx
@@ -6,9 +6,11 @@ import MetTable from 'components/common/Table';
 import { User } from 'models/user';
 import { HeadCell, PaginationOptions } from 'components/common/Table/types';
 import { ActionContext } from './UserActionProvider';
+import { When } from 'react-if';
 
 export const UserDetails = () => {
-    const { savedUser, setAddUserModalOpen } = useContext(ActionContext);
+    const { pageInfo, paginationOptions, setPaginationOptions, users, savedUser, setAddUserModalOpen, isUserLoading } =
+        useContext(ActionContext);
     const [superUserAssigned, setSuperUser] = useState(false);
     const [deactivatedUser, setDeactivatedUser] = useState(false);
     const handleToggleChange = (event: ChangeEvent<HTMLInputElement>, checked: boolean) => {
@@ -51,7 +53,7 @@ export const UserDetails = () => {
         <MetPageGridContainer>
             <Grid container direction="row" item rowSpacing={1} sx={{ mb: 4 }}>
                 <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={2} sx={{ mr: 1 }}>
+                    <Grid item xs={1.5} sx={{ mr: 1 }}>
                         <MetLabel>User:</MetLabel>
                     </Grid>
                     <Grid item xs={2}>
@@ -60,7 +62,7 @@ export const UserDetails = () => {
                 </Grid>
 
                 <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={2} sx={{ mr: 1 }}>
+                    <Grid item xs={1.5} sx={{ mr: 1 }}>
                         <MetLabel>Role:</MetLabel>
                     </Grid>
                     <Grid item xs={2}>
@@ -78,16 +80,16 @@ export const UserDetails = () => {
                 </Grid>
 
                 <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={2} sx={{ mr: 1 }}>
+                    <Grid item xs={1.5} sx={{ mr: 1 }}>
                         <MetLabel>Status:</MetLabel>
                     </Grid>
-                    <Grid item xs={2}>
+                    <Grid item xs={1.5}>
                         <MetParagraph sx={{ pl: 2 }}>{savedUser && savedUser?.email_id}</MetParagraph>
                     </Grid>
                 </Grid>
 
                 <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={4}>
+                    <Grid item xs={6}>
                         <FormControlLabel
                             control={<Switch checked={superUserAssigned} onChange={handleToggleChange} />}
                             label={<MetLabel>Assign Superuser Role</MetLabel>}
@@ -96,38 +98,41 @@ export const UserDetails = () => {
                 </Grid>
 
                 <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={2} sx={{ mr: 1 }}>
+                    <Grid item xs={1.5} sx={{ mr: 1 }}>
                         <MetLabel>Date Added:</MetLabel>
                     </Grid>
-                    <Grid item xs={2}>
+                    <Grid item xs={4}>
                         <MetParagraph sx={{ pl: 2 }}>{savedUser && savedUser?.created_date}</MetParagraph>
                     </Grid>
                 </Grid>
-                <Grid container direction="row" item xs={6} spacing={1}>
-                    <Grid item xs={4}>
-                        <FormControlLabel
-                            control={<Switch checked={superUserAssigned} onChange={handleUserDeactivated} />}
-                            label={<MetLabel>Deactivate User</MetLabel>}
-                        />
+                <When condition={savedUser && savedUser.groups && savedUser?.groups.includes('SuperUser')}>
+                    <Grid container direction="row" item xs={6} spacing={1}>
+                        <Grid item xs={6}>
+                            <FormControlLabel
+                                control={<Switch checked={deactivatedUser} onChange={handleUserDeactivated} />}
+                                label={<MetLabel>Deactivate User</MetLabel>}
+                            />
+                        </Grid>
                     </Grid>
+                </When>
+            </Grid>
+            <Grid container item justifyContent={'flex-end'} alignItems={'flex-end'} xs={12}>
+                <Grid item xs={6}></Grid>
+                <Grid container justifyContent={'flex-end'} alignItems={'flex-end'} item xs={6}>
+                    <PrimaryButton onClick={() => setAddUserModalOpen(true)}>+ Add an Engagement</PrimaryButton>
                 </Grid>
             </Grid>
-            <Grid item xs={6}></Grid>
-            <Grid item xs={6}></Grid>
-            <Grid item xs={6}>
-                <PrimaryButton onClick={() => setAddUserModalOpen(true)}>+ Add an Engagement</PrimaryButton>
-            </Grid>
-            <Grid item xs={12} lg={10}>
-                {/* <MetTable
+            <Grid item xs={12}>
+                <MetTable
                     headCells={headCells}
                     rows={users}
                     handleChangePagination={(paginationOptions: PaginationOptions<User>) =>
                         setPaginationOptions(paginationOptions)
                     }
                     paginationOptions={paginationOptions}
-                    loading={usersLoading}
+                    loading={isUserLoading}
                     pageInfo={pageInfo}
-                /> */}
+                />
             </Grid>
         </MetPageGridContainer>
     );
