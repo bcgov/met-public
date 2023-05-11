f0059984acbe2eee71e0307a4e7d020fbe6ef432
Tiago Graf <60719756+tiagograf-AOT@users.noreply.github.com>
2023-05-11 10:28:43 -0700

    Fixes user role (#1591)
    * Fixes user role
    
    Removes unused side menu items
    
    * Update user_service.py

--- met-web/src/components/layout/SideNav/SideNavElements.tsx
+++ met-web/src/components/layout/SideNav/SideNavElements.tsx
@@ -2,8 +2,6 @@ export const Routes = [
     { name: 'Home', path: '/', base: '/' },
     { name: 'Engagements', path: '/engagements', base: '/engagements' },
     { name: 'Surveys', path: '/surveys', base: '/surveys' },
-    { name: 'Calendar', path: '/calendar', base: '/calendar' },
-    { name: 'Reporting', path: '/reporting', base: '/reporting' },
     { name: 'User Management', path: '/usermanagement', base: 'usermanagement' },
     { name: 'Feedback Tool', path: '/feedback', base: 'feedback' },
 ];
