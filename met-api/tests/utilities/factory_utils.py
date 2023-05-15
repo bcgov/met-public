ccb74196c535599c0c4f7751168b2fbc58b07cdb
Tiago Graf <tiagograf@gmail.com>
2023-05-15 13:15:21 -0700

    fix unit tests

--- met-api/tests/utilities/factory_utils.py
+++ met-api/tests/utilities/factory_utils.py
@@ -114,7 +114,8 @@ def factory_engagement_model(eng_info: dict = TestEngagementInfo.engagement1, st
         status_id=status if status else eng_info.get('status'),
         start_date=eng_info.get('start_date'),
         end_date=eng_info.get('end_date'),
-        tenant_id=eng_info.get('tenant_id')
+        tenant_id=eng_info.get('tenant_id'),
+        is_internal=eng_info.get('is_internal')
     )
     db.session.add(engagement)
     db.session.commit()
