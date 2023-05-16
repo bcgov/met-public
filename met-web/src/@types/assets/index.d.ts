8058f8626866a865ac2f33ab5a29d75e5c3caa79
djnunez-aot <david.nunez@aot-technologies.com>
2023-05-16 12:29:37 -0400

    update modules

--- met-web/src/@types/assets/index.d.ts
+++ met-web/src/@types/assets/index.d.ts
@@ -6,3 +6,8 @@ declare module '*.svg' {
 }
 
 declare module '@types/arcgis-core';
+
+declare module '*.png' {
+    const value: string;
+    return value;
+}
--- met-web/src/@types/index.d.ts
+++ met-web/src/@types/index.d.ts
@@ -10,8 +10,3 @@ declare module '*.svg' {
     const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
     export default content;
 }
-
-declare module '*.png' {
-    const value: any;
-    export = value;
-}
