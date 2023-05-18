b94dde499d9d26852abcc0e40fb3ffc9afe5f526
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2023-05-18 14:53:52 -0700

    updated

--- analytics-api/src/analytics_api/models/user_feedback.py
+++ analytics-api/src/analytics_api/models/user_feedback.py
@@ -14,7 +14,7 @@ class UserFeedback(BaseModel):  # pylint: disable=too-few-public-methods
     __tablename__ = 'user_feedback'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    survey_id = db.Column(db.Integer, ForeignKey('survey.id', ondelete='CASCADE'), nullable=False)
+    survey_id = db.Column(db.Integer, ForeignKey('survey.id', ondelete='CASCADE'), primary_key=True, nullable=False)
     user_id = db.Column(db.Integer)
     comment = db.Column(db.Text())
     sentiment_analysis = db.Column(db.String(100))
