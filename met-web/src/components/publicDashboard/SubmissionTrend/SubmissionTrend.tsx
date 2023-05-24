7d0ef55ed7e5b9e32dc57c9057066436d6428bed
djnunez-aot <david.nunez@aot-technologies.com>
2023-05-24 16:23:46 -0400

    update responsiveness

--- met-web/src/components/publicDashboard/SubmissionTrend/SubmissionTrend.tsx
+++ met-web/src/components/publicDashboard/SubmissionTrend/SubmissionTrend.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, LabelList } from 'recharts';
 import { Stack, useMediaQuery, Theme, Grid, ToggleButtonGroup, ToggleButton, CircularProgress } from '@mui/material';
-import { MetPaper, MetLabel } from 'components/common';
+import { MetPaper, MetLabel, PrimaryButton } from 'components/common';
 import { Unless } from 'react-if';
 import { DASHBOARD } from '../constants';
 import { ErrorBox } from '../ErrorBox';
@@ -12,11 +12,10 @@ import {
 import { createDefaultByMonthData } from '../../../models/analytics/userResponseDetail';
 import { Engagement } from 'models/engagement';
 import { Palette } from 'styles/Theme';
-import { DateCalendar } from '@mui/x-date-pickers-pro';
+import { DatePicker } from '@mui/x-date-pickers-pro';
 import { LocalizationProvider } from '@mui/x-date-pickers-pro';
 import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
 import dayjs, { Dayjs } from 'dayjs';
-import { DateRange } from '@mui/x-date-pickers-pro';
 import { Then, If, Else } from 'react-if';
 import { formatDate } from 'components/common/dateHelper';
 
@@ -63,11 +62,21 @@ const SubmissionTrend = ({ engagement, engagementIsLoading }: SubmissionTrendPro
 
     useEffect(() => {
         fetchData();
-    }, [engagement.id]);
+    }, [engagement.id, chartBy]);
 
     useEffect(() => {
         if (fromDate && toDate) fetchData();
     }, [fromDate, toDate]);
+
+    const handleToggleChange = (event: React.MouseEvent<HTMLElement>, chartByValue: string) => {
+        setChartBy(chartByValue);
+    };
+
+    const clearDates = () => {
+        setFromDate(null);
+        setToDate(null);
+        fetchData();
+    };
 
     if (engagementIsLoading) {
         return (
@@ -98,65 +107,113 @@ const SubmissionTrend = ({ engagement, engagementIsLoading }: SubmissionTrendPro
         <>
             <MetLabel mb={2}>Live Activity - Engagement</MetLabel>
             <MetPaper sx={{ p: 2 }}>
-                <Grid container>
-                    <Grid alignItems={'center'} justifyContent={'center'} container item xs={4}>
-                        <LocalizationProvider dateAdapter={AdapterDayjs}>
-                            <Stack direction="row">
-                                <DateCalendar
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <Grid container>
+                        <Grid
+                            alignItems={'center'}
+                            justifyContent={'center'}
+                            direction="row"
+                            container
+                            item
+                            rowSpacing={0.5}
+                            lg={2}
+                            md={3}
+                        >
+                            <Grid item xs={8}>
+                                <DatePicker
                                     value={fromDate}
+                                    label="From Date"
                                     onChange={(newDate: Dayjs | null) => setFromDate(newDate)}
                                 />
-                                <DateCalendar value={toDate} onChange={(newDate: Dayjs | null) => setToDate(newDate)} />
-                            </Stack>
-                        </LocalizationProvider>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <If condition={isLoading}>
-                            <Then>
-                                <ResponsiveContainer width="100%" height={isSmallScreen ? 200 : 250}>
-                                    <BarChart
-                                        data={data}
-                                        margin={
-                                            !isSmallScreen
-                                                ? { top: 10, right: 30, left: 0, bottom: 0 }
-                                                : { top: 5, right: 0, left: -20, bottom: 0 }
-                                        }
+                            </Grid>
+                            <Grid item xs={8}>
+                                <DatePicker
+                                    label="To Date"
+                                    value={toDate}
+                                    onChange={(newDate: Dayjs | null) => setToDate(newDate)}
+                                />
+                            </Grid>
+
+                            <Grid item xs={8}>
+                                <PrimaryButton sx={{ width: '100%' }} onClick={clearDates}>
+                                    Clear
+                                </PrimaryButton>
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Stack direction={{ xs: 'column', sm: 'row' }} width="100%" justifyContent="flex-end">
+                                <ToggleButtonGroup value={chartBy} exclusive onChange={handleToggleChange}>
+                                    <ToggleButton
+                                        value="monthly"
+                                        sx={{
+                                            '&.Mui-selected': {
+                                                backgroundColor: Palette.primary.main,
+                                                color: 'white',
+                                            },
+                                        }}
                                     >
-                                        <XAxis dataKey="showdataby" />
-                                        <YAxis />
-                                        <Unless condition={isSmallScreen}>
-                                            <Tooltip />
-                                        </Unless>
-                                        <Bar
-                                            dataKey="responses"
-                                            fill={DASHBOARD.BAR_CHART.FILL_COLOR}
-                                            minPointSize={2}
-                                            barSize={50}
+                                        Monthly
+                                    </ToggleButton>
+                                    <ToggleButton
+                                        value="weekly"
+                                        sx={{
+                                            '&.Mui-selected': {
+                                                backgroundColor: Palette.primary.main,
+                                                color: 'white',
+                                            },
+                                        }}
+                                    >
+                                        Weekly
+                                    </ToggleButton>
+                                </ToggleButtonGroup>
+                            </Stack>
+                            <If condition={!isLoading}>
+                                <Then>
+                                    <ResponsiveContainer width="100%" height={isSmallScreen ? 200 : 250}>
+                                        <BarChart
+                                            data={data}
+                                            margin={
+                                                !isSmallScreen
+                                                    ? { top: 10, right: 30, left: 0, bottom: 0 }
+                                                    : { top: 5, right: 0, left: -20, bottom: 0 }
+                                            }
                                         >
-                                            <LabelList
+                                            <XAxis dataKey="showdataby" />
+                                            <YAxis />
+                                            <Unless condition={isSmallScreen}>
+                                                <Tooltip />
+                                            </Unless>
+                                            <Bar
                                                 dataKey="responses"
-                                                position="insideTop"
-                                                style={{ fill: 'white' }}
-                                            />
-                                        </Bar>
-                                    </BarChart>
-                                </ResponsiveContainer>
-                            </Then>
-                            <Else>
-                                <Grid
-                                    container
-                                    alignItems="center"
-                                    justifyContent="center"
-                                    direction="row"
-                                    width={'100%'}
-                                    height={HEIGHT}
-                                >
-                                    <CircularProgress color="inherit" />
-                                </Grid>
-                            </Else>
-                        </If>
+                                                fill={DASHBOARD.BAR_CHART.FILL_COLOR}
+                                                minPointSize={2}
+                                                barSize={50}
+                                            >
+                                                <LabelList
+                                                    dataKey="responses"
+                                                    position="insideTop"
+                                                    style={{ fill: 'white' }}
+                                                />
+                                            </Bar>
+                                        </BarChart>
+                                    </ResponsiveContainer>
+                                </Then>
+                                <Else>
+                                    <Grid
+                                        container
+                                        alignItems="center"
+                                        justifyContent="center"
+                                        direction="row"
+                                        width={'100%'}
+                                        height={isSmallScreen ? 200 : 250}
+                                    >
+                                        <CircularProgress color="inherit" />
+                                    </Grid>
+                                </Else>
+                            </If>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </LocalizationProvider>
             </MetPaper>
         </>
     );
