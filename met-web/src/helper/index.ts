2fb2fe6c2d0906b37b9f3d60c9945a18cb85e892
Tiago Graf <60719756+tiagograf-AOT@users.noreply.github.com>
2023-05-26 16:51:59 -0700

    Add tenant id to urls (#1641)
    * Add tenant id to urls
    
    * Update PublicHeader.tsx
    
    * Fix tests
    
    * Update factory_utils.py
    
    * Update PublicHeader.tsx
    
    * Add helper to tenant url
    
    * Lintfix
    
    * Add set global tenant
    
    * Fix tests
    
    * Fix tests
    
    * Update notification.py
    
    * Update factory_utils.py
    
    * Fix tests
    
    * Update test_survey.py
    
    * Update test_survey.py
    
    * Lintfix
    
    * fix import isort
    
    * Update factory_scenarios.py
    
    * Update factory_scenarios.py
    
    * Update factory_scenarios.py
    
    * Fix tests
    
    * Attempt to fix global tenant tests
    
    * Unit tests missing tenant id
    
    * Update test_survey.py
    
    * lint fix

--- met-web/src/helper/index.ts
+++ met-web/src/helper/index.ts
@@ -1,11 +1,13 @@
-const replaceUrl = (URL: string, key: string, value: string) => {
+import { AppConfig } from 'config';
+
+export const replaceUrl = (URL: string, key: string, value: string) => {
     return URL.replace(key, value);
 };
 
 interface Params {
     [param: string]: string;
 }
-const replaceAllInURL = ({ URL, params }: { URL: string; params: Params }) => {
+export const replaceAllInURL = ({ URL, params }: { URL: string; params: Params }) => {
     const regex = new RegExp(Object.keys(params).join('|'), 'gi');
 
     return URL.replace(regex, function (matched) {
@@ -13,7 +15,14 @@ const replaceAllInURL = ({ URL, params }: { URL: string; params: Params }) => {
     });
 };
 
-const filterQueryParams = (queryParams: { [x: string]: unknown }) => {
+export const getBaseUrl = () => {
+    const tenantId = sessionStorage.getItem('tenantId');
+    const domain = AppConfig.publicUrl ? AppConfig.publicUrl : window.location.origin;
+    const baseUrl = `${domain}/${tenantId}`;
+    return baseUrl;
+};
+
+export const filterQueryParams = (queryParams: { [x: string]: unknown }) => {
     const filteredQueryParams: { [x: string]: unknown } = {};
     Object.keys(queryParams).forEach((key) => {
         if (queryParams[key]) {
@@ -22,5 +31,3 @@ const filterQueryParams = (queryParams: { [x: string]: unknown }) => {
     });
     return filteredQueryParams;
 };
-
-export { replaceUrl, replaceAllInURL, filterQueryParams };
