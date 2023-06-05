4c289fc5ec8431126602023a6babf817e683c579
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2023-06-05 06:00:13 -0700

    Email Queue Model and utility method

--- met-api/src/met_api/models/email_queue.py
+++ met-api/src/met_api/models/email_queue.py
@@ -4,6 +4,10 @@ Manages the Email queue
 """
 from __future__ import annotations
 
+from typing import List
+
+from sqlalchemy import null, or_
+
 from .base_model import BaseModel
 from .db import db
 
@@ -14,8 +18,17 @@ class EmailQueue(BaseModel):  # pylint: disable=too-few-public-methods
     __tablename__ = 'email_queue'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    entity_id = db.Column(db.Integer, nullable=False)  # id of the entity which triggers email
+    entity_id = db.Column(db.Integer, nullable=False)  # id of the entity which triggers notificatio
     entity_type = db.Column(db.String(100),
                             nullable=False)  # type of the entity which triggers email ,like engagement , user
     action = db.Column(db.String(100))  # created , deleted etc
     notification_status = db.Column(db.String(50))
+
+    @staticmethod
+    def get_unprocessed_mails(max_size: int) -> List[EmailQueue]:
+        """Return a list of unprocessed emails."""
+        query = EmailQueue.query.filter(
+            or_(EmailQueue.notification_status.is_(null()), EmailQueue.notification_status == ''))
+        if max_size != 0:
+            query = query.limit(max_size)
+        return query.all()
