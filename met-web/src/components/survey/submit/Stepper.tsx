eae868b12e4933143a402ba1d578f1c317896247
djnunez-aot <david.nunez@aot-technologies.com>
2023-06-12 12:02:27 -0400

    remove theme provider

--- met-web/src/components/survey/submit/Stepper.tsx
+++ met-web/src/components/survey/submit/Stepper.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Stepper, Step, StepLabel, Box } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
 import { FormInfo } from 'components/Form/types';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -16,17 +15,15 @@ function FormStepper({ currentPage, totalPages, pages }: ProgressBarProps) {
     const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <ThemeProvider theme={theme}>
-            <Box sx={{ pb: 2 }}>
-                <Stepper activeStep={currentPage} alternativeLabel>
-                    {pages.map((page: FormInfo, index: number) => (
-                        <Step key={index}>
-                            <StepLabel> {(!isMobile || index === currentPage) && page.title}</StepLabel>
-                        </Step>
-                    ))}
-                </Stepper>
-            </Box>
-        </ThemeProvider>
+        <Box sx={{ pb: 2 }}>
+            <Stepper activeStep={currentPage} alternativeLabel>
+                {pages.map((page: FormInfo, index: number) => (
+                    <Step key={index}>
+                        <StepLabel> {(!isMobile || index === currentPage) && page.title}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+        </Box>
     );
 }
 
