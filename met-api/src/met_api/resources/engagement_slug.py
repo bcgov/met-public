2c46ab40e4d0277632aa1d1b1485250b32e86ee3
Jad <jad.saad@aot-technologies.com>
2023-06-20 15:46:23 -0700

    make CORS safe

--- met-api/src/met_api/resources/engagement_slug.py
+++ met-api/src/met_api/resources/engagement_slug.py
@@ -7,6 +7,7 @@ from flask_restx import Namespace, Resource
 from met_api.auth import auth
 from met_api.exceptions.business_exception import BusinessException
 from met_api.services.engagement_slug import EngagementSlugService
+from met_api.utils.util import allowedorigins, cors_preflight
 
 API = Namespace('engagementslugs', description='Endpoints for Engagement Slug Management')
 
@@ -16,7 +17,7 @@ class EngagementSlug(Resource):
     """Resource for managing an engagement slug."""
 
     @staticmethod
-    @cross_origin()
+    @cross_origin(origins=allowedorigins())
     def get(slug):
         """Fetch an engagement slug matching the provided slug."""
         try:
@@ -26,7 +27,7 @@ class EngagementSlug(Resource):
             return {'error': str(e)}, HTTPStatus.NOT_FOUND
 
     @staticmethod
-    @cross_origin()
+    @cross_origin(origins=allowedorigins())
     @auth.require
     def patch(slug):
         """Update an existing engagement slug."""
