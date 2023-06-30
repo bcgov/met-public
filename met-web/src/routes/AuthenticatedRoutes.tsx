78642cd7546caf456603b7f6a6c57b6ad57076ff
Jad <jad.saad@aot-technologies.com>
2023-06-30 16:59:49 -0700

    Remove dummy route

--- met-web/src/routes/AuthenticatedRoutes.tsx
+++ met-web/src/routes/AuthenticatedRoutes.tsx
@@ -29,7 +29,6 @@ const AuthenticatedRoutes = () => {
             <ScrollToTop />
             <Routes>
                 <Route path="/" element={<Dashboard />} />
-                <Route path="/asdasds" element={<Dashboard />} />
                 <Route path="/engagements" element={<EngagementListing />} />
                 <Route path="/surveys" element={<SurveyListing />} />
                 <Route path="/surveys/create" element={<CreateSurvey />} />
