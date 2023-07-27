1c2f3a2a91bf4fd9947b5631592315c231d02982
Jad <jad.saad@aot-technologies.com>
2023-07-27 14:31:21 -0700

    remove commented out text

--- met-web/src/i18n.ts
+++ met-web/src/i18n.ts
@@ -1,14 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-// const enTranslation = require('./locales/en/eao.json');
-
 i18n.use(initReactI18next).init({
-    // resources: {
-    //     en: {
-    //         translation: enTranslation,
-    //     },
-    // },
     lng: 'en',
     fallbackLng: 'en',
     interpolation: {
