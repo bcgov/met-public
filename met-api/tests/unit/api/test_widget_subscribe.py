0b1ee8fc3bdc41b0cbae7cd621db259f2e784e33
djnunez-aot <david.nunez@aot-technologies.com>
2023-07-27 16:38:02 -0400

    update test file

--- met-api/tests/unit/api/test_widget_subscribe.py
+++ met-api/tests/unit/api/test_widget_subscribe.py
@@ -1,24 +1,20 @@
 """Tests to verify the Widget Subscribe API end-point.
-
 Test-Suite to ensure that the Widget Subscribe API endpoint is working as expected.
 """
 import json
-
 from faker import Faker
-
 from met_api.utils.enums import ContentType
 from tests.utilities.factory_scenarios import TestSubscribeInfo, TestJwtClaims, TestWidgetInfo
 from tests.utilities.factory_utils import factory_auth_header, factory_engagement_model, factory_widget_model
 
 fake = Faker()
 
-
 def test_create_subscribe(client, jwt, session):  # pylint:disable=unused-argument
     """Assert that widget subscribe can be POSTed."""
     engagement = factory_engagement_model()
     TestWidgetInfo.widget1['engagement_id'] = engagement.id
     widget = factory_widget_model(TestWidgetInfo.widget1)
-    subscribe_info = TestSubscribeInfo.subscribe_info_1
+    subscribe_info = TestSubscribeInfo.subscribe_info_1.value
     headers = factory_auth_header(jwt=jwt, claims=TestJwtClaims.no_role)
 
     data = {
@@ -36,7 +32,7 @@ def test_create_subscribe(client, jwt, session):  # pylint:disable=unused-argume
     assert rv.json.get('type') == subscribe_info.get('type')
     response_subscribe_items = rv.json.get('subscribe_items')
     assert len(response_subscribe_items) == 1
-    assert response_subscribe_items[0].get('description') == subscribe_info.get('items')[0].get('description')
+    assert response_subscribe_items[0].get('description') == subscribe_info.get('description')
 
 
 def test_get_subscribe(client, jwt, session):  # pylint:disable=unused-argument
@@ -44,9 +40,9 @@ def test_get_subscribe(client, jwt, session):  # pylint:disable=unused-argument
     engagement = factory_engagement_model()
     TestWidgetInfo.widget_subscribe['engagement_id'] = engagement.id
     widget = factory_widget_model(TestWidgetInfo.widget_subscribe)
-    subscribe_info = {"widget_id":1,"type":"EMAIL_LIST","items":[{"description":"{\"blocks\":[{\"key\":\"8vfub\",\"text\":\"test description\",\"type\":\"unstyled\",\"depth\":0,\"inlineStyleRanges\":[],\"entityRanges\":[],\"data\":{}}],\"entityMap\":{}}","call_to_action_type":"link","call_to_action_text":"Click here to sign up","form_type":"EMAIL_LIST"}]}
+    subscribe_info = TestSubscribeInfo.subscribe_info_1.value
     headers = factory_auth_header(jwt=jwt, claims=TestJwtClaims.no_role)
-    
+
     data = {
         **subscribe_info,
         'widget_id': widget.id,
@@ -65,6 +61,4 @@ def test_get_subscribe(client, jwt, session):  # pylint:disable=unused-argument
         content_type=ContentType.JSON.value
     )
     assert rv.status_code == 200
-    assert len(rv.json) == 1, 'One Widget Subscribe Should exist.'
-    assert rv.json[0].get('type') == subscribe_info.get('type')
-
+    assert rv.json.get('type') == subscribe_info.get('type')
