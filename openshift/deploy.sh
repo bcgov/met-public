c80907aca695009b8409a79e713781314c9c6fe2
Aaron Forde <aaron.forde@aot-technologies.com>
2023-08-02 18:56:25 -0700

    making changes to deploy file again

--- openshift/deploy.sh
+++ openshift/deploy.sh
@@ -1,3 +1,4 @@
+oc project $2-tools
 
 oc tag met-api:latest met-api:$1
 oc tag notify-api:latest notify-api:$1
@@ -6,3 +7,6 @@ oc tag met-cron:latest met-cron:$1
 oc tag met-web:latest met-web:$1
 oc tag met-analytics:latest met-analytics:$1
 oc tag dagster-etl:latest dagster-etl:$1
+
+oc rollout status dc/met-api -n $2-$1 -w
+oc rollout status dc/met-web -n $2-$1 -w
