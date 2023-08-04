5d12a0e957c2ecd9cfe5672627fde635fab9aeeb
djnunez-aot <103138766+djnunez-aot@users.noreply.github.com>
2023-08-04 13:01:08 -0400

    Description fix for subscription drawer (#1963)
    * update email text
    
    * hard coded text
    
    * linting & adding tenant_name
    
    * add get_tenant name method to submission service
    
    * update text

--- met-web/src/components/engagement/form/EngagementWidgets/Subscribe/SubscribeForm.tsx
+++ met-web/src/components/engagement/form/EngagementWidgets/Subscribe/SubscribeForm.tsx
@@ -26,10 +26,8 @@ const Form = () => {
                 <Grid item xs={12}>
                     <MetParagraph>
                         The email list will collect email addresses for a mailing list. A "double-opt-in" email will be
-                        sent to confirm the subscription.Only the email addresses that have been double-opted-in will be
-                        on the list. Please include the unsubscribe link provided on the Email List screen in every
-                        future communication. Unsubscribed email addresses will be removed from the list. Please
-                        downloaded the list before each communication.
+                        sent to confirm the subscription. Only the email addresses that have been double-opted-in will
+                        be on the list.
                     </MetParagraph>
                 </Grid>
                 <Grid item xs={12} marginBottom="1em">
