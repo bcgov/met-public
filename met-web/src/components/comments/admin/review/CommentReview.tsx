30d31ebd45d3f0d7485ec385d6a16b715132b9a9
djnunez-aot <david.nunez@aot-technologies.com>
2023-08-10 16:18:26 -0400

    update text on comment review page

--- met-web/src/components/comments/admin/review/CommentReview.tsx
+++ met-web/src/components/comments/admin/review/CommentReview.tsx
@@ -373,8 +373,9 @@ const CommentReview = () => {
                                         }
                                     />
                                     <MetParagraph sx={{ marginLeft: '3em', color: '#707070', fontSize: '13px' }}>
-                                        This will be inserted in the email sent to the respondent in the following
-                                        sentence: One of your comments can't be published because of "other"
+                                        This will be inserted in the email sent to the respondent: We have reviewed your
+                                        feedback and can't accept it for the following reason(s): - Your feedback
+                                        contains "other"
                                     </MetParagraph>
                                     <TextField
                                         disabled={!hasOtherReason}
