92502f330796337239a3418eec26db3c654dc3b5
djnunez-aot <david.nunez@aot-technologies.com>
2023-08-10 16:20:33 -0400

    update comment review page

--- met-web/src/components/comments/admin/review/CommentReview.tsx
+++ met-web/src/components/comments/admin/review/CommentReview.tsx
@@ -373,9 +373,11 @@ const CommentReview = () => {
                                         }
                                     />
                                     <MetParagraph sx={{ marginLeft: '3em', color: '#707070', fontSize: '13px' }}>
-                                        This will be inserted in the email sent to the respondent: We have reviewed your
-                                        feedback and can't accept it for the following reason(s): - Your feedback
-                                        contains "other"
+                                        This will be inserted in the email sent to the respondent:
+                                        <MetParagraph sx={{ fontStyle: 'italic' }}>
+                                            We have reviewed your feedback and can't accept it for the following
+                                            reason(s): - Your feedback contains "other"
+                                        </MetParagraph>
                                     </MetParagraph>
                                     <TextField
                                         disabled={!hasOtherReason}
