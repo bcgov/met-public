28fb5e78164128c45d2556b414e5c59990ba08e3
Jad <jad.saad@aot-technologies.com>
2023-09-07 11:53:14 -0700

    Contain rich text editor to the form sign up drawer

--- met-web/src/components/engagement/form/EngagementWidgets/Subscribe/FormSignUpDrawer.tsx
+++ met-web/src/components/engagement/form/EngagementWidgets/Subscribe/FormSignUpDrawer.tsx
@@ -2,21 +2,27 @@ import React, { useContext, useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Divider from '@mui/material/Divider';
-import { Grid, FormGroup, FormControlLabel, Radio } from '@mui/material';
+import { Grid, FormControlLabel, Radio, FormLabel, FormControl } from '@mui/material';
 import { MetHeader3, MetLabel, PrimaryButton, SecondaryButton } from 'components/common';
-import { useForm, FormProvider, SubmitHandler, Controller, ControllerRenderProps } from 'react-hook-form';
+import { useForm, FormProvider, SubmitHandler, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import { SubscribeContext } from './SubscribeContext';
 import ControlledTextField from 'components/common/ControlledInputComponents/ControlledTextField';
 import { SUBSCRIBE_TYPE } from 'models/subscription';
 import RichTextEditor from 'components/common/RichTextEditor';
+import ControlledRadioGroup from 'components/common/ControlledInputComponents/ControlledRadioGroup';
+import { Palette } from 'styles/Theme';
 
 const schema = yup
     .object({
-        description: yup.string().max(500, 'Description cannot exceed 500 characters'),
-        call_to_action_type: yup.string(),
-        call_to_action_text: yup.string().max(25, 'call to action cannot exceed 25 characters'),
+        description: yup.string().max(5, 'Description cannot exceed 500 characters'),
+        richDescription: yup.string(),
+        call_to_action_type: yup.string().required('This field is required'),
+        call_to_action_text: yup
+            .string()
+            .required('This field is required')
+            .max(25, 'call to action cannot exceed 25 characters'),
     })
     .required();
 
@@ -25,33 +31,30 @@ type FormSignUp = yup.TypeOf<typeof schema> & {
 };
 
 const FormSignUpDrawer = () => {
-    const { handleSubscribeDrawerOpen, formSignUpTabOpen, richFormSignUpDescription, setRichFormSignUpDescription } =
-        useContext(SubscribeContext);
+    const { handleSubscribeDrawerOpen, formSignUpTabOpen } = useContext(SubscribeContext);
     const [isCreating] = useState(false);
-    const [initialRichDescription, setInitialRichDescription] = useState('');
     const methods = useForm<FormSignUp>({
         resolver: yupResolver(schema),
     });
 
-    useEffect(() => {
-        methods.setValue('description', '');
-        methods.setValue('call_to_action_type', 'link');
-        methods.setValue('call_to_action_text', '');
-        setInitialRichDescription(richFormSignUpDescription);
-    }, []);
+    // useEffect(() => {
+    //     methods.setValue('description', '');
+    //     methods.setValue('call_to_action_type', 'link');
+    //     methods.setValue('call_to_action_text', '');
+    // }, []);
 
-    const { handleSubmit } = methods;
+    const {
+        handleSubmit,
+        trigger,
+        control,
+        setValue,
+        formState: { errors },
+    } = methods;
 
     const onSubmit: SubmitHandler<FormSignUp> = async (data: FormSignUp) => {
+        trigger();
+        console.log(data);
         return;
-    };
-
-    const handleDescriptionChange = (rawText: string) => {
-        return rawText;
-    };
-
-    const handleRichDescriptionChange = (newState: string) => {
-        setRichFormSignUpDescription(newState);
     };
 
     return (
@@ -64,101 +67,94 @@ const FormSignUpDrawer = () => {
         >
             <Box sx={{ width: '40vw', paddingTop: '7em' }} role="presentation">
                 <FormProvider {...methods}>
-                    <form onSubmit={handleSubmit(onSubmit)}>
-                        <Grid
-                            container
-                            direction="row"
-                            alignItems="baseline"
-                            justifyContent="flex-start"
-                            spacing={2}
-                            padding="2em"
-                        >
-                            <Grid item xs={12}>
-                                <MetHeader3 bold>Form Sign-Up</MetHeader3>
-                                <Divider sx={{ marginTop: '1em' }} />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MetLabel sx={{ marginBottom: '2px' }}>Description</MetLabel>
-                                <RichTextEditor
+                    <Grid
+                        container
+                        direction="row"
+                        alignItems="baseline"
+                        justifyContent="flex-start"
+                        spacing={2}
+                        padding="2em"
+                    >
+                        <Grid item xs={12}>
+                            <MetHeader3 bold>Form Sign-Up</MetHeader3>
+                            <Divider sx={{ marginTop: '1em' }} />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <MetLabel sx={{ marginBottom: '2px' }}>Description</MetLabel>
+                            {/* <RichTextEditor
                                     setRawText={handleDescriptionChange}
                                     handleEditorStateChange={handleRichDescriptionChange}
                                     initialRawEditorState={initialRichDescription || ''}
                                     error={false}
                                     helperText={'Maximum 550 Characters.'}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MetLabel sx={{ marginBottom: '2px' }}>Call-to-action type</MetLabel>
-                                <FormGroup>
-                                    <Controller
-                                        control={methods.control}
-                                        name="call_to_action_type"
-                                        render={({
-                                            field,
-                                        }: {
-                                            field: ControllerRenderProps<FormSignUp, 'call_to_action_type'>;
-                                        }) => (
-                                            <FormControlLabel
-                                                control={<Radio />}
-                                                label="Link"
-                                                checked={field.value === 'link'}
-                                                onChange={() => field.onChange('link')}
-                                            />
-                                        )}
+                                /> */}
+                            <Controller
+                                name="richDescription"
+                                control={control}
+                                render={({ field }) => (
+                                    <RichTextEditor
+                                        handleEditorStateChange={(editorState: string) => field.onChange(editorState)}
+                                        setRawText={(rawText: string) => setValue('description', rawText)}
+                                        error={Boolean(errors.description)}
+                                        helperText={String(errors.description?.message)}
                                     />
-                                    <Controller
-                                        control={methods.control}
-                                        name="call_to_action_type"
-                                        render={({
-                                            field,
-                                        }: {
-                                            field: ControllerRenderProps<FormSignUp, 'call_to_action_type'>;
-                                        }) => (
-                                            <FormControlLabel
-                                                control={<Radio />}
-                                                label="Button"
-                                                checked={field.value === 'button'}
-                                                onChange={() => field.onChange('button')}
-                                            />
-                                        )}
-                                    />
-                                </FormGroup>
+                                )}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <FormControl>
+                                <FormLabel
+                                    id="controlled-radio-buttons-group"
+                                    sx={{ fontWeight: 'bold', color: Palette.text.primary, paddingBottom: 1 }}
+                                >
+                                    Call-to-action Type
+                                </FormLabel>
+                                <ControlledRadioGroup name="call_to_action_type">
+                                    <FormControlLabel value="link" control={<Radio />} label={'Link'} />
+                                    <FormControlLabel value="button" control={<Radio />} label={'Button'} />
+                                </ControlledRadioGroup>
+                            </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <MetLabel sx={{ marginBottom: '2px' }}>Call-to-action</MetLabel>
+                            <ControlledTextField
+                                name="call_to_action_text"
+                                variant="outlined"
+                                label=""
+                                InputLabelProps={{
+                                    shrink: false,
+                                }}
+                                fullWidth
+                                size="small"
+                            />
+                        </Grid>
+                        <Grid
+                            item
+                            xs={12}
+                            container
+                            direction="row"
+                            spacing={1}
+                            justifyContent={'flex-start'}
+                            marginTop="2em"
+                        >
+                            <Grid item>
+                                <PrimaryButton
+                                    onClick={handleSubmit(onSubmit)}
+                                    type="submit"
+                                    loading={isCreating}
+                                >{`Save & Close`}</PrimaryButton>
                             </Grid>
-                            <Grid item xs={12}>
-                                <MetLabel sx={{ marginBottom: '2px' }}>Call-to-action</MetLabel>
-                                <ControlledTextField
-                                    name="call_to_action_text"
-                                    variant="outlined"
-                                    label=""
-                                    InputLabelProps={{
-                                        shrink: false,
-                                    }}
-                                    fullWidth
-                                    size="small"
-                                />
-                            </Grid>
-                            <Grid
-                                item
-                                xs={12}
-                                container
-                                direction="row"
-                                spacing={1}
-                                justifyContent={'flex-start'}
-                                marginTop="2em"
-                            >
-                                <Grid item>
-                                    <PrimaryButton type="submit" loading={isCreating}>{`Save & Close`}</PrimaryButton>
-                                </Grid>
-                                <Grid item>
-                                    <SecondaryButton
-                                        onClick={() => handleSubscribeDrawerOpen(SUBSCRIBE_TYPE.EMAIL_LIST, false)}
-                                    >
-                                        Cancel
-                                    </SecondaryButton>
-                                </Grid>
+                            <Grid item>
+                                <SecondaryButton
+                                    onClick={() => handleSubscribeDrawerOpen(SUBSCRIBE_TYPE.EMAIL_LIST, false)}
+                                >
+                                    Cancel
+                                </SecondaryButton>
                             </Grid>
                         </Grid>
-                    </form>
+                    </Grid>
                 </FormProvider>
             </Box>
         </Drawer>
