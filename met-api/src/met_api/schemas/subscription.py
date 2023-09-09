88a6b118630d6379a75e104704e80c82991d1feb
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2023-09-09 02:53:10 -0700

    updating type column

--- met-api/src/met_api/schemas/subscription.py
+++ met-api/src/met_api/schemas/subscription.py
@@ -4,6 +4,9 @@ Manages the subscription
 """
 
 from marshmallow import EXCLUDE, Schema, fields
+from marshmallow_enum import EnumField
+
+from met_api.constants.subscription_type import SubscriptionTypes
 
 
 class SubscriptionSchema(Schema):
@@ -19,7 +22,7 @@ class SubscriptionSchema(Schema):
     participant_id = fields.Int(data_key='participant_id')
     is_subscribed = fields.Bool(data_key='is_subscribed')
     project_id = fields.Str(data_key='project_id')
-    type = fields.Str(data_key='type')
+    type = EnumField(SubscriptionTypes, by_value=True)
     created_by = fields.Str(data_key='created_by')
     created_date = fields.Str(data_key='created_date')
     updated_by = fields.Str(data_key='updated_by')
