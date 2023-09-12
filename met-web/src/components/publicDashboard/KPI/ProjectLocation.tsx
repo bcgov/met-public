6df783e189e1964f10a1ad80582d01513ab727f6
djnunez-aot <david.nunez@aot-technologies.com>
2023-09-12 09:02:11 -0400

    update conditional in project location

--- met-web/src/components/publicDashboard/KPI/ProjectLocation.tsx
+++ met-web/src/components/publicDashboard/KPI/ProjectLocation.tsx
@@ -23,7 +23,7 @@ const ProjectLocation = ({ engagement, engagementIsLoading, handleProjectMapData
     const [isError, setIsError] = useState(false);
     const isTablet = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
     const circleSize = isTablet ? 100 : 250;
-
+    const mapExists = data?.latitude !== null && data?.longitude !== null;
     const setErrors = (error: AxiosError) => {
         if (error.response?.status !== HTTP_STATUS_CODES.NOT_FOUND) {
             setIsError(true);
@@ -57,25 +57,27 @@ const ProjectLocation = ({ engagement, engagementIsLoading, handleProjectMapData
     if (isLoading || engagementIsLoading) {
         return (
             <>
-                <Grid item sm={8} md={4} sx={{ width: isTablet ? '90%' : '100%' }}>
-                    <MetLabel mb={2}>Project Location</MetLabel>
-                    <MetPaper sx={{ p: 2, textAlign: 'center' }}>
-                        <Stack alignItems="center" gap={1}>
-                            <Grid
-                                container
-                                alignItems="center"
-                                justifyContent="center"
-                                direction="row"
-                                width={'100%'}
-                                height={circleSize}
-                            >
-                                <CircularProgress color="inherit" />
-                            </Grid>
-                        </Stack>
-                    </MetPaper>
-                </Grid>
+                <MetLabel mb={2}>Project Location</MetLabel>
+                <MetPaper sx={{ p: 2, textAlign: 'center' }}>
+                    <Stack alignItems="center" gap={1}>
+                        <Grid
+                            container
+                            alignItems="center"
+                            justifyContent="center"
+                            direction="row"
+                            width={'100%'}
+                            height={circleSize}
+                        >
+                            <CircularProgress color="inherit" />
+                        </Grid>
+                    </Stack>
+                </MetPaper>
             </>
         );
+    }
+
+    if (!mapExists) {
+        return <></>;
     }
 
     if (isError) {
@@ -89,30 +91,32 @@ const ProjectLocation = ({ engagement, engagementIsLoading, handleProjectMapData
         );
     }
 
-    if (!data) {
-        return <></>;
+    if (data) {
+        return (
+            <>
+                <Grid item sm={8} md={4} sx={{ width: isTablet ? '90%' : '100%' }}>
+                    <MetLabel mb={{ md: 0.5, lg: 2 }}>Project Location</MetLabel>
+                    <MetPaper sx={{ textAlign: 'center' }}>
+                        <Box
+                            sx={{
+                                width: '100%',
+                                height: '280px',
+                            }}
+                        >
+                            <MetMap
+                                geojson={geoJSONDecode(data.geojson)}
+                                latitude={data.latitude}
+                                longitude={data.longitude}
+                                markerLabel={data.marker_label}
+                            />
+                        </Box>
+                    </MetPaper>
+                </Grid>
+            </>
+        );
     }
 
-    return (
-        <Grid item sm={8} md={4} sx={{ width: isTablet ? '90%' : '100%' }}>
-            <MetLabel mb={{ md: 0.5, lg: 2 }}>Project Location</MetLabel>
-            <MetPaper sx={{ textAlign: 'center' }}>
-                <Box
-                    sx={{
-                        width: '100%',
-                        height: '280px',
-                    }}
-                >
-                    <MetMap
-                        geojson={geoJSONDecode(data?.geojson)}
-                        latitude={data.latitude}
-                        longitude={data.longitude}
-                        markerLabel={data?.marker_label}
-                    />
-                </Box>
-            </MetPaper>
-        </Grid>
-    );
+    return <></>;
 };
 
 export default ProjectLocation;
