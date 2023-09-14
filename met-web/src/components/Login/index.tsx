5e18b1a9b21a1cd90a2fe7fd7bb15ff32626e7ae
Jad <jad.saad@aot-technologies.com>
2023-09-14 15:37:48 -0700

    remove useless import

--- met-web/src/components/Login/index.tsx
+++ met-web/src/components/Login/index.tsx
@@ -3,10 +3,8 @@ import UserService from 'services/userService';
 import { Grid } from '@mui/material';
 import { useAppSelector } from 'hooks';
 import { PrimaryButton } from 'components/common';
-import { useNavigate } from 'react-router-dom';
 const Login = () => {
     const isLoggedIn = useAppSelector((state) => state.user.authentication.authenticated);
-    const navigate = useNavigate();
 
     return (
         <Grid container direction="row" justifyContent="flex-start" alignItems="flex-start" spacing={2} padding="2em">
