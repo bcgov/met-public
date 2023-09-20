f5d59567da9400c460e98f1b13badd2322ff5e49
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2023-09-20 11:34:33 -0700

    changed CORS to allowedorigins to support multiple urls

--- met-api/src/met_api/__init__.py
+++ met-api/src/met_api/__init__.py
@@ -51,8 +51,7 @@ def create_app(run_mode=os.getenv('FLASK_ENV', 'development')):
     # All configuration are in config file
     app.config.from_object(get_named_config(run_mode))
 
-    allowed_origins = os.getenv('CORS_ORIGIN', '')
-    CORS(app, origins=allowed_origins, supports_credentials=True)
+    CORS(app, origins=allowedorigins(), supports_credentials=True)
 
     # Register blueprints
     app.register_blueprint(API_BLUEPRINT)
