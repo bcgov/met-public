aed3a035bed1244ce79d30d58da3016cb5e9b805
djnunez-aot <david.nunez@aot-technologies.com>
2023-09-25 13:00:36 -0400

    add listener to update submit button

--- met-web/src/components/FormioModalListener/index.tsx
+++ met-web/src/components/FormioModalListener/index.tsx
@@ -5,9 +5,34 @@ const FormioModalListener = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {
+        const formioElements = document.querySelectorAll('.formio-form');
+
+        const observer = new MutationObserver(() => {
+            formioElements.forEach((formio) => {
+                const submitButton = formio.querySelector('button.btn-wizard-nav-submit');
+                if (submitButton) {
+                    submitButton.textContent = 'Submit';
+                }
+            });
+        });
+
+        formioElements.forEach((formio) => {
+            observer.observe(formio, { childList: true, subtree: true });
+        });
+
+        // Disconnect observer on component unmount to avoid memory leaks
+        return () => observer.disconnect();
+    }, []);
+
+    useEffect(() => {
         const formioModal = document.querySelector('.formio-dialog');
         if (formioModal) {
             formioModal.remove();
+        }
+
+        const submitButton = document.querySelector('button.btn-wizard-nav-submit');
+        if (submitButton) {
+            submitButton.textContent = 'Submit';
         }
     }, [pathname]);
 
