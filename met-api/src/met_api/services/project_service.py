b52fb8aa6c1e17fa9a8c983b27fafb5173e2bd51
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2023-09-27 09:46:20 -0700

    updated payload for epic

--- met-api/src/met_api/services/project_service.py
+++ met-api/src/met_api/services/project_service.py
@@ -27,7 +27,6 @@ class ProjectService:
             engagement, engagement_metadata = ProjectService._get_engagement_and_metadata(eng_id)
 
             epic_comment_period_payload = ProjectService._construct_epic_payload(engagement, project_id)
-            logger.error('epic_comment_period_payload: %s', epic_comment_period_payload)
 
             eao_service_account_token = ProjectService._get_eao_service_account_token()
 
