cd56d6f3417ea32c6ca10ca9f06396e2c04fadcf
djnunez-aot <david.nunez@aot-technologies.com>
2023-09-28 14:40:09 -0400

    remove unecessary listener

--- met-web/src/components/FormioListener/index.tsx
+++ met-web/src/components/FormioListener/index.tsx
@@ -5,26 +5,6 @@ const FormioListener = () => {
     const { pathname } = useLocation();
 
     useEffect(() => {
-        const formioElements = document.querySelectorAll('.formio-form');
-
-        const observer = new MutationObserver(() => {
-            formioElements.forEach((formio) => {
-                const submitButton = formio.querySelector('button.btn-wizard-nav-submit');
-                if (submitButton) {
-                    submitButton.textContent = 'Submit';
-                }
-            });
-        });
-
-        formioElements.forEach((formio) => {
-            observer.observe(formio, { childList: true, subtree: true });
-        });
-
-        // Disconnect observer on component unmount to avoid memory leaks
-        return () => observer.disconnect();
-    }, []);
-
-    useEffect(() => {
         const formioModal = document.querySelector('.formio-dialog');
         if (formioModal) {
             formioModal.remove();
