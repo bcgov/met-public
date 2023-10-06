2c0eef37e94670a6ccaf4084b6364fdf9fca5739
jadmsaadaot <91914654+jadmsaadaot@users.noreply.github.com>
2023-10-06 14:39:53 -0700

    Tweaks to AutoSave snackbar (#2305)

--- met-web/src/components/survey/building/AutoSaveSnackBar.tsx
+++ met-web/src/components/survey/building/AutoSaveSnackBar.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { Alert, Snackbar, Stack } from '@mui/material';
 import PublishedWithChangesIcon from '@mui/icons-material/PublishedWithChanges';
 
+const FORMIO_MODAL_Z_INDEX = 10000;
 export const AutoSaveSnackBar = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
     return (
         <Snackbar
             anchorOrigin={{
-                vertical: 'bottom',
+                vertical: 'top',
                 horizontal: 'right',
             }}
             open={open}
-            autoHideDuration={1000}
+            autoHideDuration={2500}
             onClose={handleClose}
+            sx={{
+                zIndex: FORMIO_MODAL_Z_INDEX + 1,
+            }}
         >
             <Alert
                 data-testid="alert-notification"
