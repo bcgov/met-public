1be6e78a2b831153e51be6994ebeb25ea3db18f6
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2023-11-23 15:27:17 -0800

    display comments with associated question

--- met-api/src/met_api/services/comment_service.py
+++ met-api/src/met_api/services/comment_service.py
@@ -82,7 +82,7 @@ class CommentService:
         """Get comments paginated."""
         include_unpublished = CommentService.can_view_unapproved_comments(survey_id)
 
-        comment_schema = CommentSchema(many=True, only=('text', 'submission_date'))
+        comment_schema = CommentSchema(many=True, only=('text', 'submission_date', 'label', 'submission_id'))
         items, total = Comment.get_accepted_comments_by_survey_id_paginated(
             survey_id, pagination_options, search_text, include_unpublished)
         return {
