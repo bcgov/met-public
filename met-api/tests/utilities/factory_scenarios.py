e5efd89851b5e5e2cdec30d2c48a006a4e7015f5
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2024-01-12 23:37:09 -0800

    Updating factory scenarios

--- met-api/tests/utilities/factory_scenarios.py
+++ met-api/tests/utilities/factory_scenarios.py
@@ -270,8 +270,10 @@ class TestJwtClaims(dict, Enum):
         'firstname': fake.first_name(),
         'lastname': fake.last_name(),
         'preferred_username': fake.user_name(),
-        'client_roles': [
+        'realm_access': {
+            'roles': [
             ]
+        }
     }
 
     public_user_role = {
@@ -282,9 +284,11 @@ class TestJwtClaims(dict, Enum):
         'preferred_username': fake.user_name(),
         'email': fake.email(),
         'tenant_id': 1,
-        'client_roles': [
+        'realm_access': {
+            'roles': [
                 'public_user'
             ]
+        }
     }
 
     met_admin_role = {
@@ -297,7 +301,8 @@ class TestJwtClaims(dict, Enum):
         'tenant_id': 1,
         'email': 'staff@gov.bc.ca',
         'identity_provider': LoginSource.IDIR.value,
-        'client_roles': [
+        'realm_access': {
+            'roles': [
                 'staff',
                 'view_engagement',
                 'create_survey',
@@ -309,6 +314,7 @@ class TestJwtClaims(dict, Enum):
                 'update_user_group',
                 'create_tenant'
             ]
+        }
     }
 
     staff_admin_role = {
@@ -321,7 +327,8 @@ class TestJwtClaims(dict, Enum):
         'tenant_id': 1,
         'email': 'staff@gov.bc.ca',
         'identity_provider': LoginSource.IDIR.value,
-        'client_roles': [
+        'realm_access': {
+            'roles': [
                 'staff',
                 'view_engagement',
                 'create_engagement',
@@ -346,6 +353,7 @@ class TestJwtClaims(dict, Enum):
                 'export_proponent_comment_sheet',
                 'export_internal_comment_sheet'
             ]
+        }
     }
     team_member_role = {
         'iss': CONFIG.JWT_OIDC_TEST_ISSUER,
@@ -357,13 +365,15 @@ class TestJwtClaims(dict, Enum):
         'email': 'staff@gov.bc.ca',
         'identity_provider': LoginSource.IDIR.value,
         'tenant_id': 1,
-        'client_roles': [
+        'realm_access': {
+            'roles': [
                 'staff',
                 'view_engagement',
                 'view_users',
                 'clone_survey',
                 'export_proponent_comment_sheet'
             ]
+        }
     }
 
     reviewer_role = {
@@ -376,10 +386,12 @@ class TestJwtClaims(dict, Enum):
         'email': 'staff@gov.bc.ca',
         'identity_provider': LoginSource.IDIR.value,
         'tenant_id': 1,
-        'client_roles': [
+        'realm_access': {
+            'roles': [
                 'staff',
                 'view_users',
             ]
+        }
     }
 
 
