b6973b22eb1793fb6def9e3010cede59e46eee60
NatSquared <nat.k.weiland@gmail.com>
2024-01-22 17:37:53 -0800

    Remove "test role claims" config option This setting was made redundant by PR#2630

--- met-api/src/met_api/config.py
+++ met-api/src/met_api/config.py
@@ -349,9 +349,6 @@ class TestConfig(TestKeyConfig, Config):  # pylint: disable=too-few-public-metho
         self.KC['BASE_URL'] = os.getenv('KEYCLOAK_TEST_BASE_URL', self.KC['BASE_URL'])
         self.KC['REALMNAME'] = os.getenv('KEYCLOAK_TEST_REALMNAME', self.KC['REALMNAME'])
 
-        JWT_OIDC_TEST_CLAIMS = os.getenv('JWT_OIDC_TEST_CLAIMS', 'realm_access.roles')
-        self.JWT_CONFIG['ROLE_CLAIM'] = JWT_OIDC_TEST_CLAIMS
-
     # Propagate exceptions up to the test runner
     TESTING = env_truthy('TESTING', default=True)
 
