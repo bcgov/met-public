11808ad9b88f412d039fe6ee54f8d4b46c99e1b2
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2024-02-05 20:22:52 -0800

    Updating met api notification

--- met-api/src/met_api/utils/notification.py
+++ met-api/src/met_api/utils/notification.py
@@ -12,12 +12,13 @@ from met_api.services.rest_service import RestService
 def get_tenant_site_url(tenant_id, path=''):
     """Get the tenant specific site url (domain / tenant / path)."""
     is_single_tenant_environment = current_app.config.get('IS_SINGLE_TENANT_ENVIRONMENT', False)
-    site_url = current_app.config.get('SITE_URL', '')
+    paths = current_app.config['PATH_CONFIG']
+    site_url = paths.get('SITE', '')
     if not is_single_tenant_environment:
         if tenant_id is None:
             raise ValueError('Missing tenant id.')
         tenant: Tenant = Tenant.find_by_id(tenant_id)
-        return site_url + f'/{tenant.short_name}' + path
+        return site_url + f'/{tenant.short_name.lower()}' + path
     else:
         return site_url + path
 
