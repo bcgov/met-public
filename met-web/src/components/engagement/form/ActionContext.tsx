1293375a215c47cf7a987b8c97a187c06e203b50
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2024-03-04 17:12:47 -0800

    Updating comment

--- met-web/src/components/engagement/form/ActionContext.tsx
+++ met-web/src/components/engagement/form/ActionContext.tsx
@@ -53,7 +53,7 @@ export const ActionContext = createContext<EngagementContext>({
     loadingAuthorization: true,
     isNewEngagement: true,
     setIsNewEngagement: () => {
-        throw new Error('setIsNewEngagement is unimplemented');
+        /* empty default method  */
     },
 });
 
