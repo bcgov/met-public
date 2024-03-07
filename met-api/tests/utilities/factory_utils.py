2c27f27fb7725ca0ab3066a04df45fb295024e78
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2024-03-06 16:06:33 -0800

    DESENG-511 : Added Unit test Survey translation

--- met-api/tests/utilities/factory_utils.py
+++ met-api/tests/utilities/factory_utils.py
@@ -26,14 +26,23 @@ from met_api.constants.email_verification import EmailVerificationType
 from met_api.constants.engagement_status import Status
 from met_api.constants.widget import WidgetType
 from met_api.models import Tenant
+from met_api.models import language
 from met_api.models.comment import Comment as CommentModel
-from met_api.models.email_verification import EmailVerification as EmailVerificationModel
+from met_api.models.email_verification import (
+    EmailVerification as EmailVerificationModel,
+)
 from met_api.models.engagement import Engagement as EngagementModel
-from met_api.models.engagement_metadata import EngagementMetadata, MetadataTaxon
+from met_api.models.engagement_metadata import (
+    EngagementMetadata,
+    MetadataTaxon,
+)
 from met_api.models.engagement_settings import EngagementSettingsModel
-from met_api.models.engagement_slug import EngagementSlug as EngagementSlugModel
+from met_api.models.engagement_slug import (
+    EngagementSlug as EngagementSlugModel,
+)
 from met_api.models.feedback import Feedback as FeedbackModel
 from met_api.models.language import Language as LanguageModel
+from met_api.models.survey_translation import SurveyTranslation as SurveyTranslationModel
 from met_api.models.membership import Membership as MembershipModel
 from met_api.models.participant import Participant as ParticipantModel
 from met_api.models.poll_answers import PollAnswer as PollAnswerModel
@@ -45,20 +54,44 @@ from met_api.models.subscription import Subscription as SubscriptionModel
 from met_api.models.survey import Survey as SurveyModel
 from met_api.models.timeline_event import TimelineEvent as TimelineEventModel
 from met_api.models.widget import Widget as WidgetModal
-from met_api.models.widget_documents import WidgetDocuments as WidgetDocumentModel
+from met_api.models.widget_documents import (
+    WidgetDocuments as WidgetDocumentModel,
+)
 from met_api.models.widget_item import WidgetItem as WidgetItemModal
 from met_api.models.widget_map import WidgetMap as WidgetMapModel
 from met_api.models.widget_poll import Poll as WidgetPollModel
-from met_api.models.widget_timeline import WidgetTimeline as WidgetTimelineModel
+from met_api.models.widget_timeline import (
+    WidgetTimeline as WidgetTimelineModel,
+)
 from met_api.models.widget_video import WidgetVideo as WidgetVideoModel
 from met_api.utils.constants import TENANT_ID_HEADER
 from met_api.utils.enums import MembershipStatus
 from tests.utilities.factory_scenarios import (
-    TestCommentInfo, TestEngagementInfo, TestEngagementMetadataInfo, TestEngagementMetadataTaxonInfo,
-    TestEngagementSlugInfo, TestFeedbackInfo, TestJwtClaims, TestLanguageInfo, TestParticipantInfo, TestPollAnswerInfo,
-    TestPollResponseInfo, TestReportSettingInfo, TestSubmissionInfo, TestSurveyInfo, TestTenantInfo, TestTimelineInfo,
-    TestUserInfo, TestWidgetDocumentInfo, TestWidgetInfo, TestWidgetItemInfo, TestWidgetMap, TestWidgetPollInfo,
-    TestWidgetVideo)
+    TestCommentInfo,
+    TestEngagementInfo,
+    TestEngagementMetadataInfo,
+    TestEngagementMetadataTaxonInfo,
+    TestEngagementSlugInfo,
+    TestFeedbackInfo,
+    TestJwtClaims,
+    TestLanguageInfo,
+    TestParticipantInfo,
+    TestPollAnswerInfo,
+    TestPollResponseInfo,
+    TestReportSettingInfo,
+    TestSubmissionInfo,
+    TestSurveyInfo,
+    TestTenantInfo,
+    TestTimelineInfo,
+    TestUserInfo,
+    TestWidgetDocumentInfo,
+    TestWidgetInfo,
+    TestWidgetItemInfo,
+    TestWidgetMap,
+    TestWidgetPollInfo,
+    TestWidgetVideo,
+    TestSurveyTranslationInfo,
+)
 
 
 fake = Faker()
@@ -68,7 +101,7 @@ CONFIG = get_named_config('testing')
 JWT_HEADER = {
     'alg': CONFIG.JWT_OIDC_TEST_ALGORITHMS,
     'typ': 'JWT',
-    'kid': CONFIG.JWT_OIDC_TEST_AUDIENCE
+    'kid': CONFIG.JWT_OIDC_TEST_AUDIENCE,
 }
 
 
@@ -87,7 +120,7 @@ def factory_survey_model(survey_info: dict = TestSurveyInfo.survey1):
         created_date=survey_info.get('created_date'),
         updated_date=survey_info.get('updated_date'),
         is_hidden=survey_info.get('is_hidden'),
-        is_template=survey_info.get('is_template')
+        is_template=survey_info.get('is_template'),
     )
     survey.save()
     return survey
@@ -105,7 +138,7 @@ def factory_survey_and_eng_model(survey_info: dict = TestSurveyInfo.survey1):
         updated_date=survey_info.get('updated_date'),
         is_hidden=survey_info.get('is_hidden'),
         is_template=survey_info.get('is_template'),
-        engagement_id=eng.id
+        engagement_id=eng.id,
     )
     survey.save()
     return survey, eng
@@ -142,7 +175,9 @@ def factory_email_verification(survey_id, type=None):
     return email_verification
 
 
-def factory_engagement_model(eng_info: dict = TestEngagementInfo.engagement1, name=None, status=None):
+def factory_engagement_model(
+    eng_info: dict = TestEngagementInfo.engagement1, name=None, status=None
+):
     """Produce a engagement model."""
     engagement = EngagementModel(
         name=name if name else fake.name(),
@@ -155,7 +190,7 @@ def factory_engagement_model(eng_info: dict = TestEngagementInfo.engagement1, na
         status_id=status if status else eng_info.get('status'),
         start_date=eng_info.get('start_date'),
         end_date=eng_info.get('end_date'),
-        is_internal=eng_info.get('is_internal')
+        is_internal=eng_info.get('is_internal'),
     )
     if tenant_id := eng_info.get('tenant_id'):
         engagement.tenant_id = tenant_id
@@ -177,7 +212,8 @@ def factory_tenant_model(tenant_info: dict = TestTenantInfo.tenant1):
 
 
 def factory_engagement_metadata_model(
-        metadata_info: dict = TestEngagementMetadataInfo.metadata0):
+    metadata_info: dict = TestEngagementMetadataInfo.metadata0,
+):
     """Produce a test-ready engagement metadata model."""
     metadata = EngagementMetadata(
         engagement_id=metadata_info.get('engagement_id'),
@@ -192,13 +228,19 @@ def factory_metadata_requirements(auth: Optional[Auth] = None):
     """Create a tenant, an associated staff user, and engagement, for tests."""
     tenant = factory_tenant_model()
     tenant.short_name = fake.lexify(text='????').upper()
-    (engagement_info := TestEngagementInfo.engagement1.copy())['tenant_id'] = tenant.id
+    (engagement_info := TestEngagementInfo.engagement1.copy())[
+        'tenant_id'
+    ] = tenant.id
     engagement = factory_engagement_model(engagement_info)
     (staff_info := TestUserInfo.user_staff_1.copy())['tenant_id'] = tenant.id
     factory_staff_user_model(TestJwtClaims.staff_admin_role['sub'], staff_info)
     taxon = factory_metadata_taxon_model(tenant.id)
     if auth:
-        headers = factory_auth_header(auth, claims=TestJwtClaims.staff_admin_role, tenant_id=tenant.short_name)
+        headers = factory_auth_header(
+            auth,
+            claims=TestJwtClaims.staff_admin_role,
+            tenant_id=tenant.short_name,
+        )
         return taxon, engagement, tenant, headers
     return taxon, engagement, tenant, None
 
@@ -208,15 +250,23 @@ def factory_taxon_requirements(auth: Optional[Auth] = None):
     tenant = factory_tenant_model()
     tenant.short_name = fake.lexify(text='????').upper()
     (staff_info := TestUserInfo.user_staff_1.copy())['tenant_id'] = tenant.id
-    factory_staff_user_model(TestJwtClaims.staff_admin_role.get('sub'), staff_info)
+    factory_staff_user_model(
+        TestJwtClaims.staff_admin_role.get('sub'), staff_info
+    )
     if auth:
-        headers = factory_auth_header(auth, claims=TestJwtClaims.staff_admin_role, tenant_id=tenant.short_name)
+        headers = factory_auth_header(
+            auth,
+            claims=TestJwtClaims.staff_admin_role,
+            tenant_id=tenant.short_name,
+        )
         return tenant, headers
     return tenant, None
 
 
-def factory_metadata_taxon_model(tenant_id: int = 1,
-                                 taxon_info: dict = TestEngagementMetadataTaxonInfo.taxon1):
+def factory_metadata_taxon_model(
+    tenant_id: int = 1,
+    taxon_info: dict = TestEngagementMetadataTaxonInfo.taxon1,
+):
     """Produce a test-ready metadata taxon model."""
     taxon = MetadataTaxon(
         tenant_id=tenant_id,
@@ -232,7 +282,9 @@ def factory_metadata_taxon_model(tenant_id: int = 1,
     return taxon
 
 
-def factory_staff_user_model(external_id=None, user_info: dict = TestUserInfo.user_staff_1):
+def factory_staff_user_model(
+    external_id=None, user_info: dict = TestUserInfo.user_staff_1
+):
     """Produce a staff user model."""
     # Generate a external id if not passed
     external_id = external_id or fake.uuid4()
@@ -249,31 +301,43 @@ def factory_staff_user_model(external_id=None, user_info: dict = TestUserInfo.us
     return user
 
 
-def factory_participant_model(participant: dict = TestParticipantInfo.participant1):
+def factory_participant_model(
+    participant: dict = TestParticipantInfo.participant1,
+):
     """Produce a participant model."""
     participant = ParticipantModel(
         email_address=ParticipantModel.encode_email(
-            participant['email_address']),
+            participant['email_address']
+        ),
     )
     participant.save()
     return participant
 
 
-def factory_membership_model(user_id, engagement_id, member_type='TEAM_MEMBER', status=MembershipStatus.ACTIVE.value):
+def factory_membership_model(
+    user_id,
+    engagement_id,
+    member_type='TEAM_MEMBER',
+    status=MembershipStatus.ACTIVE.value,
+):
     """Produce a Membership model."""
-    membership = MembershipModel(user_id=user_id,
-                                 engagement_id=engagement_id,
-                                 type=member_type,
-                                 is_latest=True,
-                                 version=1,
-                                 status=status)
+    membership = MembershipModel(
+        user_id=user_id,
+        engagement_id=engagement_id,
+        type=member_type,
+        is_latest=True,
+        version=1,
+        status=status,
+    )
 
     membership.created_by_id = user_id
     membership.save()
     return membership
 
 
-def factory_feedback_model(feedback_info: dict = TestFeedbackInfo.feedback1, status=None):
+def factory_feedback_model(
+    feedback_info: dict = TestFeedbackInfo.feedback1, status=None
+):
     """Produce a feedback model."""
     feedback = FeedbackModel(
         status=feedback_info.get('status'),
@@ -289,9 +353,11 @@ def factory_feedback_model(feedback_info: dict = TestFeedbackInfo.feedback1, sta
 def factory_auth_header(jwt, claims, tenant_id=None):
     """Produce JWT tokens for use in tests."""
     return {
-        'Authorization': 'Bearer ' + jwt.create_jwt(claims=claims, header=JWT_HEADER),
-        TENANT_ID_HEADER: (tenant_id or
-                           current_app.config.get('DEFAULT_TENANT_SHORT_NAME')),
+        'Authorization': 'Bearer '
+        + jwt.create_jwt(claims=claims, header=JWT_HEADER),
+        TENANT_ID_HEADER: (
+            tenant_id or current_app.config.get('DEFAULT_TENANT_SHORT_NAME')
+        ),
     }
 
 
@@ -309,7 +375,9 @@ def factory_widget_model(widget_info: dict = TestWidgetInfo.widget1):
     return widget
 
 
-def factory_widget_item_model(widget_info: dict = TestWidgetItemInfo.widget_item1):
+def factory_widget_item_model(
+    widget_info: dict = TestWidgetItemInfo.widget_item1,
+):
     """Produce a widget model."""
     widget = WidgetItemModal(
         widget_id=widget_info.get('widget_id'),
@@ -323,8 +391,12 @@ def factory_widget_item_model(widget_info: dict = TestWidgetItemInfo.widget_item
     return widget
 
 
-def factory_submission_model(survey_id, engagement_id, participant_id,
-                             submission_info: dict = TestSubmissionInfo.submission1):
+def factory_submission_model(
+    survey_id,
+    engagement_id,
+    participant_id,
+    submission_info: dict = TestSubmissionInfo.submission1,
+):
     """Produce a submission model."""
     submission = SubmissionModel(
         survey_id=survey_id,
@@ -343,7 +415,9 @@ def factory_submission_model(survey_id, engagement_id, participant_id,
     return submission
 
 
-def factory_comment_model(survey_id, submission_id, comment_info: dict = TestCommentInfo.comment1):
+def factory_comment_model(
+    survey_id, submission_id, comment_info: dict = TestCommentInfo.comment1
+):
     """Produce a comment model."""
     comment = CommentModel(
         survey_id=survey_id,
@@ -356,7 +430,9 @@ def factory_comment_model(survey_id, submission_id, comment_info: dict = TestCom
     return comment
 
 
-def factory_document_model(document_info: dict = TestWidgetDocumentInfo.document1):
+def factory_document_model(
+    document_info: dict = TestWidgetDocumentInfo.document1,
+):
     """Produce a comment model."""
     document = WidgetDocumentModel(
         title=document_info.get('title'),
@@ -378,13 +454,16 @@ def patch_token_info(claims, monkeypatch):
         return claims
 
     monkeypatch.setattr(
-        'met_api.utils.user_context._get_token_info', token_info)
+        'met_api.utils.user_context._get_token_info', token_info
+    )
 
     # Add a database user that matches the token
     # factory_staff_user_model(external_id=claims.get('sub'))
 
 
-def factory_engagement_slug_model(eng_slug_info: dict = TestEngagementSlugInfo.slug1):
+def factory_engagement_slug_model(
+    eng_slug_info: dict = TestEngagementSlugInfo.slug1,
+):
     """Produce a engagement model."""
     slug = EngagementSlugModel(
         slug=eng_slug_info.get('slug'),
@@ -395,7 +474,9 @@ def factory_engagement_slug_model(eng_slug_info: dict = TestEngagementSlugInfo.s
     return slug
 
 
-def factory_survey_report_setting_model(report_setting_info: dict = TestReportSettingInfo.report_setting_1):
+def factory_survey_report_setting_model(
+    report_setting_info: dict = TestReportSettingInfo.report_setting_1,
+):
     """Produce a engagement model."""
     setting = ReportSettingModel(
         survey_id=report_setting_info.get('survey_id'),
@@ -426,29 +507,32 @@ def factory_poll_model(widget, poll_info: dict = TestWidgetPollInfo.poll1):
         description=poll_info.get('description'),
         status=poll_info.get('status'),
         engagement_id=widget.engagement_id,
-        widget_id=widget.id
+        widget_id=widget.id,
     )
     poll.save()
     return poll
 
 
-def factory_poll_answer_model(poll, answer_info: dict = TestPollAnswerInfo.answer1):
+def factory_poll_answer_model(
+    poll, answer_info: dict = TestPollAnswerInfo.answer1
+):
     """Produce a Poll  model."""
     answer = PollAnswerModel(
-        answer_text=answer_info.get('answer_text'),
-        poll_id=poll.id
+        answer_text=answer_info.get('answer_text'), poll_id=poll.id
     )
     answer.save()
     return answer
 
 
-def factory_poll_response_model(poll, answer, response_info: dict = TestPollResponseInfo.response1):
+def factory_poll_response_model(
+    poll, answer, response_info: dict = TestPollResponseInfo.response1
+):
     """Produce a Poll  model."""
     response = PollResponseModel(
         participant_id=response_info.get('participant_id'),
         selected_answer_id=answer.id,
         poll_id=poll.id,
-        widget_id=poll.widget_id
+        widget_id=poll.widget_id,
     )
     response.save()
     return response
@@ -466,7 +550,9 @@ def factory_video_model(video_info: dict = TestWidgetVideo.video1):
     return video
 
 
-def factory_widget_timeline_model(widget_timeline: dict = TestTimelineInfo.widget_timeline):
+def factory_widget_timeline_model(
+    widget_timeline: dict = TestTimelineInfo.widget_timeline,
+):
     """Produce a widget timeline model."""
     widget_timeline = WidgetTimelineModel(
         widget_id=widget_timeline.get('widget_id'),
@@ -478,7 +564,9 @@ def factory_widget_timeline_model(widget_timeline: dict = TestTimelineInfo.widge
     return widget_timeline
 
 
-def factory_timeline_event_model(timeline_event: dict = TestTimelineInfo.timeline_event):
+def factory_timeline_event_model(
+    timeline_event: dict = TestTimelineInfo.timeline_event,
+):
     """Produce a widget timeline model."""
     timeline_event = TimelineEventModel(
         widget_id=timeline_event.get('widget_id'),
@@ -507,11 +595,40 @@ def factory_widget_map_model(widget_map: dict = TestWidgetMap.map1):
 
 
 def factory_language_model(lang_info: dict = TestLanguageInfo.language1):
-    """Produce a Language  model."""
-    language = LanguageModel(
+    """Produce a Language model."""
+    language_model = LanguageModel(
         name=lang_info.get('name'),
         code=lang_info.get('code'),
         right_to_left=lang_info.get('right_to_left'),
     )
-    language.save()
-    return language
+    language_model.save()
+    return language_model
+
+
+def factory_survey_translation_model(
+    translate_info: dict = TestSurveyTranslationInfo.survey_translation1,
+):
+    """Produce a translation  model."""
+    translation = SurveyTranslationModel(
+        survey_id=translate_info.get('survey_id'),
+        language_id=translate_info.get('language_id'),
+        name=translate_info.get('name'),
+        form_json=translate_info.get('form_json'),
+    )
+    translation.save()
+    return translation
+
+
+def factory_survey_translation_and_engagement_model():
+    """Produce a translation model along with survey and language."""
+    survey, eng = factory_survey_and_eng_model()
+    lang = factory_language_model()
+
+    translation = SurveyTranslationModel(
+        survey_id=survey.id,
+        language_id=lang.id,
+        name=TestSurveyTranslationInfo.survey_translation1.get('name'),
+        form_json=TestSurveyTranslationInfo.survey_translation1.get('form_json'),
+    )
+    translation.save()
+    return translation, survey, lang
