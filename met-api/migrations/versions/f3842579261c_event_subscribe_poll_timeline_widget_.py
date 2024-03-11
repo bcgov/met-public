75285f65b8d064e4b3bdc704452cc9de7c7d26f8
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2024-03-11 11:34:35 -0700

    DESENG-515: Correcting migration file

--- met-api/migrations/versions/f3842579261c_event_subscribe_poll_timeline_widget_.py
+++ met-api/migrations/versions/f3842579261c_event_subscribe_poll_timeline_widget_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'f3842579261c'
-down_revision = '274a2774607b'
+down_revision = '35124d2e41cb'
 branch_labels = None
 depends_on = None
 
