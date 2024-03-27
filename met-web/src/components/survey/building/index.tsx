471e876edae7d2becebea3aa7265ad207f40a949
Ratheesh kumar R <108045773+ratheesh-aot@users.noreply.github.com>
2024-03-27 09:02:14 -0700

    [To Main ] DESENG-523:  Survey bugs fixed (#2428)
    * DESENG-523: Report Settings - Survey url fixed
    
    * DESENG-523: Survey advanced search with date issue fix
    
    * DESENG-523: Fixing advanced search survey listing
    
    * DESENG-523: Removing survey status variables from auto saving
    
    As there is a debounce function and it queue the old values of is_hidden and is_template. So to avoid that removing it from auto saving.
    
    * DESENG-523: Changelog update
    
    * Fix changelog

--- met-web/src/components/survey/building/index.tsx
+++ met-web/src/components/survey/building/index.tsx
@@ -38,8 +38,8 @@ interface SurveyForm {
     id: string;
     form_json: unknown;
     name: string;
-    is_hidden: boolean;
-    is_template: boolean;
+    is_hidden?: boolean;
+    is_template?: boolean;
 }
 
 const SurveyFormBuilder = () => {
@@ -149,8 +149,6 @@ const SurveyFormBuilder = () => {
             id: String(surveyId),
             form_json: form,
             name: name,
-            is_hidden: isHiddenSurvey,
-            is_template: isTemplateSurvey,
         });
     };
 
