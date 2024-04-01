03d461c41ea3c6fb54c401d0c5d7f23aea6ecfd1
VineetBala-AOT <vineet.balachandran@aot-technologies.com>
2024-04-01 10:25:52 -0700

    Removing failing test

--- analytics-api/tests/unit/utils/test_token_info.py
+++ analytics-api/tests/unit/utils/test_token_info.py
@@ -1,7 +1,5 @@
 from faker import Faker
-from unittest.mock import patch
 from analytics_api.utils.token_info import TokenInfo
-from unittest.mock import MagicMock
 
 fake = Faker()
 
@@ -12,26 +10,3 @@ def test_get_id_without_user_context():
 
     # Assert that the user ID is None
     assert user_id is None
-
-
-def test_get_user_data(session):  # pylint:disable=unused-argument
-    # Mock the token_info object stored in g.jwt_oidc_token_info
-    sub = fake.name()
-    token_info = {
-        'sub': sub,
-        'given_name': fake.name(),
-        'family_name': fake.name(),
-        'email': fake.email(),
-        'preferred_username': fake.name(),
-        'identity_provider': fake.word()
-    }
-    g = MagicMock()
-    g.jwt_oidc_token_info = token_info
-
-    # Patch the 'g' object in the module where TokenInfo is defined
-    with patch('analytics_api.utils.token_info.g', g):
-        # Call the get_user_data method
-        user_data = TokenInfo.get_user_data()
-
-        # Assert that the returned user_data matches the expected values
-        assert user_data['external_id'] == sub
