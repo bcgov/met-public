4b0b58f5c4f45ad9e26418692b8fba9fb9e48ed5
Ratheesh kumar R <ratheesh.kumar@aot-technologies.com>
2024-04-05 14:30:02 -0700

    DESENG-533: Fixed unit test case

--- met-api/tests/unit/utils/test_notification.py
+++ met-api/tests/unit/utils/test_notification.py
@@ -16,14 +16,31 @@
 
 Test-Suite to ensure that the Notification methods are working as expected.
 """
-
 import pytest
 
 from met_api.utils import notification
+from met_api.constants.email_verification import INTERNAL_EMAIL_DOMAIN
 
 
-@pytest.mark.parametrize('test_input_email,expected',
-                         [('helo@gw.com', True), ('helo', False), (None, False), ('', False)])
+@pytest.mark.parametrize(
+    'test_input_email,expected', [('helo@gw.com', True), ('helo', False), (None, False), ('', False)]
+)
 def test_is_valid_email(test_input_email, expected):
     """Assert that the valid email method works well.."""
     assert notification.is_valid_email(test_input_email) == expected
+
+
+@pytest.mark.parametrize(
+    'email, expected, send_email_internal_only',
+    [
+        ('test@example.com', False, True),  # External email, should be False if SEND_EMAIL_INTERNAL_ONLY is True
+        ('test' + INTERNAL_EMAIL_DOMAIN, True, True),  # Internal email, should always be True
+        ('test@example.com', True, False),  # External email, should be True if SEND_EMAIL_INTERNAL_ONLY is False
+        ('test' + INTERNAL_EMAIL_DOMAIN, True, True),  # Internal email, should always be True
+    ],
+)
+def test_is_allowed_email(app, email, expected, send_email_internal_only):
+    """Assert that the is_allowed_email method works well.."""
+    app.config['SEND_EMAIL_INTERNAL_ONLY'] = send_email_internal_only
+    with app.app_context():
+        assert notification.is_allowed_email(email) == expected
