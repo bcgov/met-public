d6af0d8ae9b839635180d01e523b5255e12e5d15
VineetBala-AOT <90332175+VineetBala-AOT@users.noreply.github.com>
2024-06-03 12:42:40 -0700

    Removing web components (#2530)
    Removed web components and all code that references it

--- met-web/src/web-components/index.ts
+++
@@ -1,6 +0,0 @@
-import EngagementTilesWC from './components/engagment-tiles-wc';
-import EngagementBannerWC from './components/engagement-banner-wc';
-export default {
-    EngagementTilesWC,
-    EngagementBannerWC,
-};
