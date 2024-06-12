c2aee8396a6f39e280123690efa8168f2cf2ecb5
NatSquared <nat.k.weiland@gmail.com>
2024-06-12 15:53:06 -0700

    Lower EngagementHero element into <main> landmark

--- met-web/src/components/engagement/new/view/index.tsx
+++ met-web/src/components/engagement/new/view/index.tsx
@@ -12,7 +12,7 @@ export const ViewEngagement = () => {
     const oldLink = `/${slug}/${language}`;
     const { engagement } = useLoaderData() as { engagement: Engagement };
     return (
-        <>
+        <main>
             <Suspense
                 fallback={
                     <Skeleton
@@ -31,63 +31,61 @@ export const ViewEngagement = () => {
                     {(resolvedEngagement: Engagement) => <EngagementHero engagement={resolvedEngagement} />}
                 </Await>
             </Suspense>
-            <main>
-                <Box
-                    sx={{
-                        background: colors.surface.blue[90],
-                        color: colors.surface.white,
-                        borderRadius: '0px 24px 0px 0px' /* upper right corner */,
-                        padding: { xs: '32px 16px', md: '32px 5vw', lg: '32px 156px' },
-                        marginTop: '-32px',
-                        zIndex: 10,
-                        position: 'relative',
-                    }}
-                >
-                    <section id="cta-section">
-                        {/* TODO: create dark theme provider for this area*/}
-                        <BodyText sx={{ color: colors.surface.white }}>
-                            You are viewing a <b>preview</b> of the new engagement page. It will replace the old page
-                            once all features are completed.
-                        </BodyText>
-                        <BodyText sx={{ color: colors.surface.white }}>
-                            For now, you can use the{' '}
-                            <Link color="white" to={oldLink}>
-                                existing page
-                            </Link>{' '}
-                            for access to all features.
-                        </BodyText>
-                        <BodyText sx={{ color: colors.surface.white }}>Work to be completed: </BodyText>
-                        <ol>
-                            <li>
-                                <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-636">
-                                    <s>Create new engagement view page</s>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-630">
-                                    <s>Add new header section to the new engagement view</s>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-631">
-                                    Add new Engagement description section
-                                </Link>
-                            </li>
-                            <li>
-                                <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-632">
-                                    Add new Dynamic pages section
-                                </Link>
-                            </li>
-                            <li>
-                                <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-633">
-                                    Add new Survey block section
-                                </Link>
-                            </li>
-                        </ol>
-                    </section>
-                </Box>
-            </main>
-        </>
+            <Box
+                sx={{
+                    background: colors.surface.blue[90],
+                    color: colors.surface.white,
+                    borderRadius: '0px 24px 0px 0px' /* upper right corner */,
+                    padding: { xs: '32px 16px', md: '32px 5vw', lg: '32px 156px' },
+                    marginTop: '-32px',
+                    zIndex: 10,
+                    position: 'relative',
+                }}
+            >
+                <section id="cta-section">
+                    {/* TODO: create dark theme provider for this area*/}
+                    <BodyText sx={{ color: colors.surface.white }}>
+                        You are viewing a <b>preview</b> of the new engagement page. It will replace the old page once
+                        all features are completed.
+                    </BodyText>
+                    <BodyText sx={{ color: colors.surface.white }}>
+                        For now, you can use the{' '}
+                        <Link color="white" to={oldLink}>
+                            existing page
+                        </Link>{' '}
+                        for access to all features.
+                    </BodyText>
+                    <BodyText sx={{ color: colors.surface.white }}>Work to be completed: </BodyText>
+                    <ol>
+                        <li>
+                            <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-636">
+                                <s>Create new engagement view page</s>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-630">
+                                <s>Add new header section to the new engagement view</s>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-631">
+                                Add new Engagement description section
+                            </Link>
+                        </li>
+                        <li>
+                            <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-632">
+                                Add new Dynamic pages section
+                            </Link>
+                        </li>
+                        <li>
+                            <Link color="white" to="https://apps.itsm.gov.bc.ca/jira/browse/DESENG-633">
+                                Add new Survey block section
+                            </Link>
+                        </li>
+                    </ol>
+                </section>
+            </Box>
+        </main>
     );
 };
 
