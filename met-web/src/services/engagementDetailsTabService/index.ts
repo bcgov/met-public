358259a796a66ccc8372b700d7b1f4a71a98a53d
Jareth Whitney <jareth.whitney@gov.bc.ca>
2025-12-08 11:54:38 -0800

    feature/deseng672: Further revisions to engagementDetailsTabService to meet SonarQubeCloud standards with promise rejection errors.

--- met-web/src/services/engagementDetailsTabService/index.ts
+++ met-web/src/services/engagementDetailsTabService/index.ts
@@ -6,7 +6,7 @@ import { EngagementDetailsTab } from 'models/engagementDetailsTab';
 export const getDetailsTabs = async (engagementId: number): Promise<EngagementDetailsTab[]> => {
     const url = replaceUrl(Endpoints.EngagementDetailsTab.GET_LIST, 'engagement_id', String(engagementId));
     if (!engagementId || Number.isNaN(Number(engagementId))) {
-        return Promise.reject('Invalid Engagement Id ' + engagementId);
+        throw new Error('Invalid Engagement Id ' + engagementId);
     }
     const response = await http.GetRequest<EngagementDetailsTab[]>(url);
     if (response.data) {
@@ -19,35 +19,27 @@ export const postDetailsTabs = async (
     engagement_id: number,
     tabs: EngagementDetailsTab[],
 ): Promise<EngagementDetailsTab[]> => {
-    try {
-        const url = replaceUrl(Endpoints.EngagementDetailsTab.CREATE, 'engagement_id', String(engagement_id));
-        const response = await http.PostRequest<EngagementDetailsTab[]>(url, tabs);
-        if (response.data) {
-            return response.data;
-        }
-        throw new Error('Failed to create engagement details tabs');
-    } catch (err) {
-        return Promise.reject(err);
+    const url = replaceUrl(Endpoints.EngagementDetailsTab.CREATE, 'engagement_id', String(engagement_id));
+    const response = await http.PostRequest<EngagementDetailsTab[]>(url, tabs);
+    if (response.data) {
+        return response.data;
     }
+    throw new Error('Failed to create engagement details tabs');
 };
 
 export const patchDetailsTabs = async (
     engagementId: number,
     tabs: EngagementDetailsTab[],
 ): Promise<EngagementDetailsTab[]> => {
-    try {
-        const url = replaceAllInURL({
-            URL: Endpoints.EngagementDetailsTab.UPDATE,
-            params: {
-                engagement_id: String(engagementId),
-            },
-        });
-        const response = await http.PutRequest<EngagementDetailsTab[]>(url, tabs);
-        if (response.data) {
-            return response.data;
-        }
-        throw new Error('Failed to update engagement details tabs');
-    } catch (err) {
-        return Promise.reject(err);
+    const url = replaceAllInURL({
+        URL: Endpoints.EngagementDetailsTab.UPDATE,
+        params: {
+            engagement_id: String(engagementId),
+        },
+    });
+    const response = await http.PutRequest<EngagementDetailsTab[]>(url, tabs);
+    if (response.data) {
+        return response.data;
     }
+    throw new Error('Failed to update engagement details tabs');
 };
