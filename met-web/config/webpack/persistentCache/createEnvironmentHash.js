b182169e8f4082c8c0746d1c54fa2abc3910a85d
NatSquared <nat.k.weiland@gmail.com>
2025-12-09 15:45:47 -0800

    Remove extra line

--- met-web/config/webpack/persistentCache/createEnvironmentHash.js
+++ met-web/config/webpack/persistentCache/createEnvironmentHash.js
@@ -2,8 +2,7 @@
 const { createHash } = require('node:crypto');
 
 module.exports = function exports (env) {
-  const hash = createHash('md5');
-  createHash()
+  const hash = createHash('sha256');
   hash.update(JSON.stringify(env));
 
   return hash.digest('hex');
