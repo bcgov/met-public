9f64009f6f98ed063294eb83c692485110c88d2b
NatSquared <nat.k.weiland@gmail.com>
2026-03-31 12:05:31 -0700

    engagement-web-dev... -> dep-web-dev.apps.gold.devops.gov.bc.ca

--- met-web/src/components/Form/formio/setup.ts
+++ met-web/src/components/Form/formio/setup.ts
@@ -1,13 +1,13 @@
 // components/Form/formio/setup.ts
 import { Formio } from '@formio/react';
-import MetFormioComponents from 'met-formio';
+import FormioComponents from 'met-formio';
 import 'components/Form/formio.scss';
 
 let initialized = false;
 
 export default function setupFormio() {
     if (initialized) return;
-    Formio.use(MetFormioComponents);
+    Formio.use(FormioComponents);
     Formio.Utils.Evaluator.noeval = false;
     initialized = true;
 }
